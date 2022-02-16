109e2f7385eb4a8d561ebdbdb5f9e0e16c29bf88
SaracenOne <SaracenOne@gmail.com>
2022-02-16 18:12:38 +0000

    Fix local variables not showing when breaking on final line
    (cherry picked from commit 657b9b659680194f6d3aa546a4b20246bb51fb39)

--- modules/gdscript/gdscript_function.cpp
+++ modules/gdscript/gdscript_function.cpp
@@ -1652,7 +1652,7 @@ void GDScriptFunction::debug_get_stack_member_state(int p_line, List<Pair<String
 	RBMap<StringName, _GDFKC> sdmap;
 	for (const List<StackDebug>::Element *E = stack_debug.front(); E; E = E->next()) {
 		const StackDebug &sd = E->get();
-		if (sd.line > p_line) {
+		if (sd.line >= p_line) {
 			break;
 		}
 
