2aa3d3250c5482ab75e6373b283e4928019e98a1
Relintai <relintai@protonmail.com>
2022-03-25 13:25:00 +0100

    Annotated the podman build all script better, also changed the log file names, and added more variants.

--- misc/build-containers/podman_build_all.sh
+++ misc/build-containers/podman_build_all.sh
@@ -15,78 +15,114 @@ img_version=pe
 
 mkdir -p logs
 
+# Windows editor (release debug)
 rm -f modules/modules_enabled.gen.h
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-windows:${img_version} scons tools=yes target=release_debug debug_symbols=no platform=windows -j4 . 2>&1 | tee logs/bew.log
 rm -f modules/modules_enabled.gen.h
 #$podman run -v ${project_root}:/root/project -w /root/project pandemonium-windows:${img_version} scons tools=yes target=debug custom_modules_shared=no debug_symbols=no platform=windows -j4 . 2>&1 | tee logs/bewd.log
 rm -f modules/modules_enabled.gen.h
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-windows:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=windows -j4 . 2>&1 | tee logs/bw.log
+
+# Windows templates 64 bit
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-windows:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=windows bits=64 -j4 . 2>&1 | tee logs/bw.log
 rm -f modules/modules_enabled.gen.h
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-windows:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=windows -j4 . 2>&1 | tee logs/bwr.log
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-windows:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=windows bits=64 -j4 . 2>&1 | tee logs/bwr.log
 rm -f modules/modules_enabled.gen.h
 
+# Windows templates 32 bit
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-windows:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=windows bits=32 -j4 . 2>&1 | tee logs/bw.log
+rm -f modules/modules_enabled.gen.h
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-windows:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=windows bits=32 -j4 . 2>&1 | tee logs/bwr.log
+rm -f modules/modules_enabled.gen.h
 
+# Linux editor
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-linux:${img_version} scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no platform=x11 -j4 . 2>&1 | tee logs/bel.log
 rm -f modules/modules_enabled.gen.h
 #$podman run -v ${project_root}:/root/project -w /root/project pandemonium-linux:${img_version} scons tools=yes target=debug custom_modules_shared=no debug_symbols=no platform=x11 -j4 . 2>&1 | tee logs/beld.log
 rm -f modules/modules_enabled.gen.h
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-linux:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=x11 -j4 . 2>&1 | tee logs/bl.log
+
+# Linux templates 64 bit
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-linux:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=x11 bits=64 -j4 . 2>&1 | tee logs/bl.log
 rm -f modules/modules_enabled.gen.h
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-linux:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=x11 -j4 . 2>&1 | tee logs/blr.log
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-linux:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=x11 bits=64 -j4 . 2>&1 | tee logs/blr.log
 rm -f modules/modules_enabled.gen.h
 
+# Linux templates 32 bit
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-linux:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=x11 bits=32 -j4 . 2>&1 | tee logs/bl.log
+rm -f modules/modules_enabled.gen.h
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-linux:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=x11 bits=32 -j4 . 2>&1 | tee logs/blr.log
+rm -f modules/modules_enabled.gen.h
 
+# Javascript editor
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-javascript:${img_version} bash -c 'source /root/emsdk_2.0.25/emsdk_env.sh;scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no threads_enabled=yes platform=javascript -j4' . 2>&1 | tee logs/bej.log
+rm -f modules/modules_enabled.gen.h
+
+# Javascript templates normal
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-javascript:${img_version} bash -c 'source /root/emsdk_2.0.25/emsdk_env.sh;scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=javascript -j4' . 2>&1 | tee logs/bj.log
 rm -f modules/modules_enabled.gen.h
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-javascript:${img_version} bash -c 'source /root/emsdk_2.0.25/emsdk_env.sh;scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=javascript -j4' . 2>&1 | tee logs/bjr.log
 rm -f modules/modules_enabled.gen.h
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-javascript:${img_version} bash -c 'source /root/emsdk_2.0.25/emsdk_env.sh;scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no threads_enabled=yes platform=javascript -j4' . 2>&1 | tee logs/bej.log
+
+# Javascript templates threads
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-javascript:${img_version} bash -c 'source /root/emsdk_2.0.25/emsdk_env.sh;scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no threads_enabled=yes platform=javascript -j4' . 2>&1 | tee logs/bj.log
+rm -f modules/modules_enabled.gen.h
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-javascript:${img_version} bash -c 'source /root/emsdk_2.0.25/emsdk_env.sh;scons tools=no target=release custom_modules_shared=no debug_symbols=no threads_enabled=yes platform=javascript -j4' . 2>&1 | tee logs/bjr.log
 rm -f modules/modules_enabled.gen.h
 
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=armv7 -j4 . 2>&1 | tee logs/ba.log
-rm -f modules/modules_enabled.gen.h
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=arm64v8 -j4 . 2>&1 | tee logs/ba.log
-rm -f modules/modules_enabled.gen.h
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=x86 -j4 . 2>&1 | tee logs/ba.log
-rm -f modules/modules_enabled.gen.h
-
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} bash -c 'cd platform/android/java/;./gradlew generatePandemoniumTemplates' . 2>&1 | tee logs/ba_gen_temp.log
-
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=android android_arch=armv7 -j4 . 2>&1 | tee logs/bar.log
-rm -f modules/modules_enabled.gen.h
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=android android_arch=arm64v8 -j4 . 2>&1 | tee logs/bar.log
-rm -f modules/modules_enabled.gen.h
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=android android_arch=x86 -j4 . 2>&1 | tee logs/bar.log
-rm -f modules/modules_enabled.gen.h
-
-$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} bash -c 'cd platform/android/java/;./gradlew generatePandemoniumTemplates' . 2>&1 | tee logs/bar_gen_temp.log
-
+# Android editor
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=armv7 -j4 . 2>&1 | tee logs/bae.log
 rm -f modules/modules_enabled.gen.h
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=arm64v8 -j4 . 2>&1 | tee logs/bae.log
 rm -f modules/modules_enabled.gen.h
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=x86 -j4 . 2>&1 | tee logs/bae.log
 rm -f modules/modules_enabled.gen.h
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=x86_64 -j4 . 2>&1 | tee logs/bae.log
+rm -f modules/modules_enabled.gen.h
 
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} bash -c 'cd platform/android/java/;./gradlew generatePandemoniumEditor' . 2>&1 | tee logs/bae_gen_temp.log
 
-#osx
+# Android templates release_debug
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=armv7 -j4 . 2>&1 | tee logs/ba.log
+rm -f modules/modules_enabled.gen.h
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=arm64v8 -j4 . 2>&1 | tee logs/ba.log
+rm -f modules/modules_enabled.gen.h
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=x86 -j4 . 2>&1 | tee logs/ba.log
+rm -f modules/modules_enabled.gen.h
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=android android_arch=x86_64 -j4 . 2>&1 | tee logs/ba.log
+rm -f modules/modules_enabled.gen.h
+
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} bash -c 'cd platform/android/java/;./gradlew generatePandemoniumTemplates' . 2>&1 | tee logs/ba_gen_temp.log
+
+# Android templates release
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=android android_arch=armv7 -j4 . 2>&1 | tee logs/bar.log
+rm -f modules/modules_enabled.gen.h
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=android android_arch=arm64v8 -j4 . 2>&1 | tee logs/bar.log
+rm -f modules/modules_enabled.gen.h
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=android android_arch=x86 -j4 . 2>&1 | tee logs/bar.log
+rm -f modules/modules_enabled.gen.h
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=android android_arch=x86_64 -j4 . 2>&1 | tee logs/bar.log
+rm -f modules/modules_enabled.gen.h
+
+$podman run -v ${project_root}:/root/project -w /root/project pandemonium-android:${img_version} bash -c 'cd platform/android/java/;./gradlew generatePandemoniumTemplates' . 2>&1 | tee logs/bar_gen_temp.log
+
+# OSX editor
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-osx:${img_version} scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no platform=osx arch=x86_64 -j4 osxcross_sdk=darwin20.4 . 2>&1 | tee logs/bex_x86_64.log
 rm -f modules/modules_enabled.gen.h
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-osx:${img_version} scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no platform=osx arch=arm64 -j4 osxcross_sdk=darwin20.4 . 2>&1 | tee logs/bex_arm64.log
 rm -f modules/modules_enabled.gen.h
 
+# OSX templates release_debug
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-osx:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=osx arch=x86_64 -j4 osxcross_sdk=darwin20.4 . 2>&1 | tee logs/bx_x86_64.log
 rm -f modules/modules_enabled.gen.h
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-osx:${img_version} scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=osx arch=arm64 -j4 osxcross_sdk=darwin20.4 . 2>&1 | tee logs/bx_arm64.log
 rm -f modules/modules_enabled.gen.h
 
+# OSX templates release
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-osx:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=osx arch=x86_64 -j4 osxcross_sdk=darwin20.4 . 2>&1 | tee logs/bxr_x86_64.log
 rm -f modules/modules_enabled.gen.h
 $podman run -v ${project_root}:/root/project -w /root/project pandemonium-osx:${img_version} scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=osx arch=arm64 -j4 osxcross_sdk=darwin20.4 . 2>&1 | tee logs/bxr_arm64.log
 rm -f modules/modules_enabled.gen.h
 
-#lipo
+# OSX lipo
 $podman run -v ${project_root}:/root/project -w /root/project/misc/osx pandemonium-osx:${img_version} bash -c ./lipo.sh
 
 #ios
@@ -104,28 +140,48 @@ files=(
   # Windows
   "pandemonium.windows.opt.64.exe"
   "pandemonium.windows.opt.debug.64.exe"
+  
+  "pandemonium.windows.opt.32.exe"
+  "pandemonium.windows.opt.debug.32.exe"
+
   "pandemonium.windows.opt.tools.64.exe"
+
   # Linux
   "pandemonium.x11.opt.64"
   "pandemonium.x11.opt.debug.64"
+
+  "pandemonium.x11.opt.32"
+  "pandemonium.x11.opt.debug.32"
+
   "pandemonium.x11.opt.tools.64"
+
   # JS
-  "pandemonium.javascript.opt.tools.threads.zip"
   "pandemonium.javascript.opt.zip"
+  "pandemonium.javascript.opt.debug.zip"
+
+  "pandemonium.javascript.opt.threads.zip"
+  "pandemonium.javascript.opt.debug.threads.zip"
+
+  "pandemonium.javascript.opt.tools.threads.zip"
+
   # Android
+  "android_source.zip"
   "android_debug.apk"
   "android_release.apk"
   "android_editor.apk"
+
   # OSX
   "pandemonium.osx.opt.arm64"
+  "pandemonium.osx.opt.universal"
+  "pandemonium.osx.opt.x86_64"
+
   "pandemonium.osx.opt.debug.arm64"
   "pandemonium.osx.opt.debug.universal"
   "pandemonium.osx.opt.debug.x86_64"
+
   "pandemonium.osx.opt.tools.arm64"
   "pandemonium.osx.opt.tools.universal"
   "pandemonium.osx.opt.tools.x86_64"
-  "pandemonium.osx.opt.universal"
-  "pandemonium.osx.opt.x86_64"
 )
 
 error=0
