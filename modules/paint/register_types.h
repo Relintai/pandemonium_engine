fe318a61ea656002d87b894fc77b822384cf701c
Relintai <relintai@protonmail.com>
2022-04-15 00:00:46 +0200

    initial setup for the paint module.

--- modules/paint/register_types.h
+++ modules/paint/register_types.h
@@ -1,7 +1,5 @@
-MIT License
-
-Copyright (c) 2019 Flairieve
-Copyright (c) 2020 cobrapitz
+/*
+Copyright (c) 2019-2022 Péter Magyar
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,3 +18,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
+
+void register_paint_types();
+void unregister_paint_types();
