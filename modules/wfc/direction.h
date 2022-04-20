837e518e5aed39d141fd450627702587efa1b811
Relintai <relintai@protonmail.com>
2022-04-20 03:05:34 +0200

    Clang format and codestyle cfixed to the wfc module.

---
+++ modules/wfc/direction.h
@@ -0,0 +1,11 @@
+#ifndef FAST_WFC_DIRECTION_HPP_
+#define FAST_WFC_DIRECTION_HPP_
+
+constexpr int directions_x[4] = { 0, -1, 1, 0 };
+constexpr int directions_y[4] = { -1, 0, 0, 1 };
+
+constexpr unsigned get_opposite_direction(unsigned direction) noexcept {
+	return 3 - direction;
+}
+
+#endif
