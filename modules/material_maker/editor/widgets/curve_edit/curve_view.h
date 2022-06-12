1943e7f150ae8af266dfd881c820373390394858
Relintai <relintai@protonmail.com>
2022-06-12 16:19:27 +0200

    Cleaned up CurveView.

--- modules/material_maker/editor/widgets/curve_edit/curve_view.h
+++ modules/material_maker/editor/widgets/curve_edit/curve_view.h
@@ -1,41 +1,38 @@
 #ifndef CURVE_VIEW_H
 #define CURVE_VIEW_H
 
+#include "core/reference.h"
+
+#include "scene/gui/control.h"
+
+class CurveBase;
 
 class CurveView : public Control {
- GDCLASS(CurveView, Control);
+	GDCLASS(CurveView, Control);
 
- public:
+public:
+	bool get_show_axes() const;
+	void set_show_axes(const bool val);
 
- Variant get_Variant();
- void set_Variant(const Variant &val);
+	Ref<CurveBase> get_curve();
+	void set_curve(const Ref<CurveBase> &val);
 
- bool get_show_axes() const;
- void set_show_axes(const bool val);
+	Vector2 transform_point(const Vector2 &p);
+	Vector2 reverse_transform_point(const Vector2 &p);
 
- Variant get_Variant();
- void set_Variant(const Variant &val);
+	void _draw();
+	void _on_resize();
 
- void _ready();
- void set_curve(const Variant &val);
- Vector2 transform_point(const Vector2 &p);
- Vector2 reverse_transform_point(const Vector2 &p);
- void _draw();
- void _on_resize();
+	CurveView();
+	~CurveView();
 
- CurveView();
- ~CurveView();
+protected:
+	void _notification(int p_what);
 
- protected:
- static void _bind_methods();
+	static void _bind_methods();
 
- //tool
- Variant  = preload("res://addons/mat_maker_gd/nodes/bases/curve_base.gd");
- //export 
- bool show_axes = false;
- //: MMCurve
- Variant ;
+	bool show_axes = false;
+	Ref<CurveBase> curve;
 };
-
 
 #endif
