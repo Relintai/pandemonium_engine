e687fc6a4a658a6f74868b284352722c98f19ab6
Relintai <relintai@protonmail.com>
2022-07-12 12:20:34 +0200

    Added a new cscript module. It's a copy of gdscript, and it's build is disabled for now. I plan to experiment with it to have a scripting language that's syntax is as close to c (c++) as possible.

---
+++ modules/cscript/config.py
@@ -0,0 +1,21 @@
+
+def can_build(env, platform):
+    #return True
+    #not yet
+    return False
+
+
+def configure(env):
+    pass
+
+
+def get_doc_classes():
+    return [
+        "@CScript",
+        "CScript",
+        "CScriptFunctionState",
+    ]
+
+
+def get_doc_path():
+    return "doc_classes"
