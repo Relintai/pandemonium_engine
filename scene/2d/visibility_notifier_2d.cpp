ad6e504a5bc944892e93baacb5d9ee7de7f92e80
BimDav <67792882+BimDav@users.noreply.github.com>
2022-07-19 10:56:47 +0200

    Add option in VisibilityEnabler2D to hide the parent for better performance

--- scene/2d/visibility_notifier_2d.cpp
+++ scene/2d/visibility_notifier_2d.cpp
@@ -169,6 +169,13 @@ void VisibilityEnabler2D::_screen_enter() {
 	if (enabler[ENABLER_PARENT_PROCESS] && get_parent()) {
 		get_parent()->set_process(true);
 	}
+	if (enabler[ENABLER_PARENT_VISIBILITY] && get_parent()) {
+		CanvasItem *ci = Object::cast_to<CanvasItem>(get_parent());
+
+		if (ci) {
+			ci->set_visible(true);
+		}
+	}
 
 	visible = true;
 }
@@ -183,6 +190,13 @@ void VisibilityEnabler2D::_screen_exit() {
 	}
 	if (enabler[ENABLER_PARENT_PROCESS] && get_parent()) {
 		get_parent()->set_process(false);
+	}
+	if (enabler[ENABLER_PARENT_VISIBILITY] && get_parent()) {
+		CanvasItem *ci = Object::cast_to<CanvasItem>(get_parent());
+
+		if (ci) {
+			ci->set_visible(false);
+		}
 	}
 
 	visible = false;
@@ -258,6 +272,14 @@ void VisibilityEnabler2D::_notification(int p_what) {
 		if (enabler[ENABLER_PARENT_PROCESS] && get_parent()) {
 			get_parent()->connect(SceneStringNames::get_singleton()->ready,
 					get_parent(), "set_process", varray(false), CONNECT_REFERENCE_COUNTED);
+		}
+		if (enabler[ENABLER_PARENT_VISIBILITY] && get_parent()) {
+			CanvasItem *ci = Object::cast_to<CanvasItem>(get_parent());
+
+			if (ci) {
+				ci->connect(SceneStringNames::get_singleton()->ready,
+						ci, "set_visible", varray(false), CONNECT_REFERENCE_COUNTED);
+			}
 		}
 	}
 
@@ -341,6 +363,7 @@ void VisibilityEnabler2D::_bind_methods() {
 	ADD_PROPERTYI(PropertyInfo(Variant::BOOL, "pause_animated_sprites"), "set_enabler", "is_enabler_enabled", ENABLER_PAUSE_ANIMATED_SPRITES);
 	ADD_PROPERTYI(PropertyInfo(Variant::BOOL, "process_parent"), "set_enabler", "is_enabler_enabled", ENABLER_PARENT_PROCESS);
 	ADD_PROPERTYI(PropertyInfo(Variant::BOOL, "physics_process_parent"), "set_enabler", "is_enabler_enabled", ENABLER_PARENT_PHYSICS_PROCESS);
+	ADD_PROPERTYI(PropertyInfo(Variant::BOOL, "visibility_parent"), "set_enabler", "is_enabler_enabled", ENABLER_PARENT_VISIBILITY);
 
 	BIND_ENUM_CONSTANT(ENABLER_PAUSE_ANIMATIONS);
 	BIND_ENUM_CONSTANT(ENABLER_FREEZE_BODIES);
@@ -348,6 +371,7 @@ void VisibilityEnabler2D::_bind_methods() {
 	BIND_ENUM_CONSTANT(ENABLER_PARENT_PROCESS);
 	BIND_ENUM_CONSTANT(ENABLER_PARENT_PHYSICS_PROCESS);
 	BIND_ENUM_CONSTANT(ENABLER_PAUSE_ANIMATED_SPRITES);
+	BIND_ENUM_CONSTANT(ENABLER_PARENT_VISIBILITY);
 	BIND_ENUM_CONSTANT(ENABLER_MAX);
 }
 
