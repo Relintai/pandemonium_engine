KleirRampage45/redot-engine

Add optional 'default' argument to get_meta()

core/object/object.cpp
@@ -922,8 +922,14 @@ void Object::set_meta(const StringName &p_name, const Variant &p_value) {
 	metadata[p_name] = p_value;
 }
 
-Variant Object::get_meta(const StringName &p_name) const {
-	ERR_FAIL_COND_V_MSG(!metadata.has(p_name), Variant(), "The object does not have any 'meta' values with the key '" + p_name + "'.");
+Variant Object::get_meta(const StringName &p_name, const Variant &p_default) const {
+	if (!metadata.has(p_name)) {
+		if (p_default != Variant()) {
+			return p_default;
+		} else {
+			ERR_FAIL_V_MSG(Variant(), "The object does not have any 'meta' values with the key '" + p_name + "'.");
+		}
+	}
 	return metadata[p_name];
 }
 
@@ -1529,7 +1535,7 @@ void Object::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("set_meta", "name", "value"), &Object::set_meta);
 	ClassDB::bind_method(D_METHOD("remove_meta", "name"), &Object::remove_meta);
-	ClassDB::bind_method(D_METHOD("get_meta", "name"), &Object::get_meta);
+	ClassDB::bind_method(D_METHOD("get_meta", "name", "default"), &Object::get_meta, DEFVAL(Variant()));
 	ClassDB::bind_method(D_METHOD("has_meta", "name"), &Object::has_meta);
 	ClassDB::bind_method(D_METHOD("get_meta_list"), &Object::_get_meta_list_bind);
 

core/object/object.h
@@ -761,7 +761,7 @@ public:
 	bool has_meta(const StringName &p_name) const;
 	void set_meta(const StringName &p_name, const Variant &p_value);
 	void remove_meta(const StringName &p_name);
-	Variant get_meta(const StringName &p_name) const;
+	Variant get_meta(const StringName &p_name, const Variant &p_default = Variant()) const;
 	void get_meta_list(List<StringName> *p_list) const;
 
 #ifdef TOOLS_ENABLED

Relintai/pandemonium_engine

(cherry picked from commit 3eb7fc4bfd376bc27ea3313ccd1da4679c48f4cb)

core/object.cpp
@@ -1061,8 +1061,15 @@ void Object::set_meta(const String &p_name, const Variant &p_value) {
 	metadata[p_name] = p_value;
 }
 
-Variant Object::get_meta(const String &p_name) const {
+Variant Object::get_meta(const String &p_name, const Variant &p_default) const {
 	ERR_FAIL_COND_V(!metadata.has(p_name), Variant());
+	if (!metadata.has(p_name)) {
+		if (p_default != Variant()) {
+			return p_default;
+		} else {
+			ERR_FAIL_V_MSG(Variant(), "The object does not have any 'meta' values with the key '" + p_name + "'.");
+		}
+	}
 	return metadata[p_name];
 }
 
@@ -1675,7 +1682,7 @@ void Object::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("set_meta", "name", "value"), &Object::set_meta);
 	ClassDB::bind_method(D_METHOD("remove_meta", "name"), &Object::remove_meta);
-	ClassDB::bind_method(D_METHOD("get_meta", "name"), &Object::get_meta);
+	ClassDB::bind_method(D_METHOD("get_meta", "name", "default"), &Object::get_meta, DEFVAL(Variant()));
 	ClassDB::bind_method(D_METHOD("has_meta", "name"), &Object::has_meta);
 	ClassDB::bind_method(D_METHOD("get_meta_list"), &Object::_get_meta_list_bind);
 

core/object.h
@@ -684,12 +684,10 @@ public:
 	void set_script(const RefPtr &p_script);
 	RefPtr get_script() const;
 
-	/* SCRIPT */
-
 	bool has_meta(const String &p_name) const;
 	void set_meta(const String &p_name, const Variant &p_value);
 	void remove_meta(const String &p_name);
-	Variant get_meta(const String &p_name) const;
+	Variant get_meta(const String &p_name, const Variant &p_default = Variant()) const;
 	void get_meta_list(List<String> *p_list) const;
 
 #ifdef TOOLS_ENABLED