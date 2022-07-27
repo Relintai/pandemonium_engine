KleirRampage45/redot-engine

Rescue orphan nodes in inherited scenes

scene/resources/packed_scene.cpp
@@ -112,6 +112,7 @@ Node *SceneState::instantiate(GenEditState p_edit_state) const {
 		const NodeData &n = nd[i];
 
 		Node *parent = nullptr;
+		String old_parent_path;
 
 		if (i > 0) {
 			ERR_FAIL_COND_V_MSG(n.parent == -1, nullptr, vformat("Invalid scene: node %s does not specify its parent node.", snames[n.name]));
@@ -119,6 +120,8 @@ Node *SceneState::instantiate(GenEditState p_edit_state) const {
 #ifdef DEBUG_ENABLED
 			if (!nparent && (n.parent & FLAG_ID_IS_PATH)) {
 				WARN_PRINT(String("Parent path '" + String(node_paths[n.parent & FLAG_MASK]) + "' for node '" + String(snames[n.name]) + "' has vanished when instancing: '" + get_path() + "'.").ascii().get_data());
+				old_parent_path = String(node_paths[n.parent & FLAG_MASK]).trim_prefix("./").replace("/", "@");
+				nparent = ret_nodes[0];
 			}
 #endif
 			parent = nparent;
@@ -330,6 +333,10 @@ Node *SceneState::instantiate(GenEditState p_edit_state) const {
 						node->_set_name_nocheck(snames[n.name]);
 					}
 				}
+			}
+
+			if (!old_parent_path.is_empty()) {
+				node->_set_name_nocheck(old_parent_path + "@" + node->get_name());
 			}
 
 			if (n.owner >= 0) {

Relintai/pandemonium_engine

(cherry picked from commit 2b53826ee6872d48acafb9ece1b9027467d2611d)

scene/resources/packed_scene.cpp
@@ -115,6 +115,7 @@ Node *SceneState::instance(GenEditState p_edit_state) const {
 		const NodeData &n = nd[i];
 
 		Node *parent = nullptr;
+		String old_parent_path;
 
 		if (i > 0) {
 			ERR_FAIL_COND_V_MSG(n.parent == -1, nullptr, vformat("Invalid scene: node %s does not specify its parent node.", snames[n.name]));
@@ -122,6 +123,8 @@ Node *SceneState::instance(GenEditState p_edit_state) const {
 #ifdef DEBUG_ENABLED
 			if (!nparent && (n.parent & FLAG_ID_IS_PATH)) {
 				WARN_PRINT(String("Parent path '" + String(node_paths[n.parent & FLAG_MASK]) + "' for node '" + String(snames[n.name]) + "' has vanished when instancing: '" + get_path() + "'.").ascii().get_data());
+				old_parent_path = String(node_paths[n.parent & FLAG_MASK]).trim_prefix("./").replace("/", "@");
+				nparent = ret_nodes[0];
 			}
 #endif
 			parent = nparent;
@@ -304,6 +307,10 @@ Node *SceneState::instance(GenEditState p_edit_state) const {
 						node->_set_name_nocheck(snames[n.name]);
 					}
 				}
+			}
+
+			if (!old_parent_path.empty()) {
+				node->_set_name_nocheck(old_parent_path + "@" + node->get_name());
 			}
 
 			if (n.owner >= 0) {