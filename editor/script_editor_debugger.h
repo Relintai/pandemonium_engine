18bb8e671a79dad255924a155119d0283a31ba31
Relintai <relintai@protonmail.com>
2022-07-27 15:07:09 +0200

    Ported:  [3.x] [Debugger] Add --debug-server CLI option. Automatically starts the editor debug server at given <IP>:<PORT>. E.g.:  # Run editor and debug server listening on any interface, port 8080 godot3 -e --path proj/proj_empty --debug-server *:8080  # Run the godot project connecting to that debug server. godot3 --path proj/proj_empty --remote-debug 127.0.0.1:8080 - Faless https://github.com/godotengine/godot/commit/225e75daba072ad31d7b7d7e74d5ca331bdf3c5d

--- editor/script_editor_debugger.h
+++ editor/script_editor_debugger.h
@@ -263,7 +263,7 @@ protected:
 	static void _bind_methods();
 
 public:
-	void start();
+	void start(int p_port = -1, const IP_Address &p_bind_address = IP_Address("*"));
 	void pause();
 	void unpause();
 	void stop();
