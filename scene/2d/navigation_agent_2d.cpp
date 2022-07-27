KleirRampage45/redot-engine

Process NavigationAgent2D/3D avoidance on demand only
Changes NavigationAgent avoidance callback to a toggle that is disabled by default.
Also fixes a few missing descriptions / wrong warnings.

scene/2d/navigation_agent_2d.cpp
@@ -37,6 +37,9 @@
 void NavigationAgent2D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_rid"), &NavigationAgent2D::get_rid);
 
+	ClassDB::bind_method(D_METHOD("set_avoidance_enabled", "enabled"), &NavigationAgent2D::set_avoidance_enabled);
+	ClassDB::bind_method(D_METHOD("get_avoidance_enabled"), &NavigationAgent2D::get_avoidance_enabled);
+
 	ClassDB::bind_method(D_METHOD("set_target_desired_distance", "desired_distance"), &NavigationAgent2D::set_target_desired_distance);
 	ClassDB::bind_method(D_METHOD("get_target_desired_distance"), &NavigationAgent2D::get_target_desired_distance);
 
@@ -82,6 +85,7 @@ void NavigationAgent2D::_bind_methods() {
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "time_horizon", PROPERTY_HINT_RANGE, "0.1,10000,0.01,suffix:s"), "set_time_horizon", "get_time_horizon");
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "max_speed", PROPERTY_HINT_RANGE, "0.1,100000,0.01,suffix:px/s"), "set_max_speed", "get_max_speed");
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "path_max_distance", PROPERTY_HINT_RANGE, "10,100,1,suffix:px"), "set_path_max_distance", "get_path_max_distance");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "avoidance_enabled"), "set_avoidance_enabled", "get_avoidance_enabled");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "navigable_layers", PROPERTY_HINT_LAYERS_2D_NAVIGATION), "set_navigable_layers", "get_navigable_layers");
 
 	ADD_SIGNAL(MethodInfo("path_changed"));
@@ -97,7 +101,7 @@ void NavigationAgent2D::_notification(int p_what) {
 			if (agent_parent != nullptr) {
 				// place agent on navigation map first or else the RVO agent callback creation fails silently later
 				NavigationServer2D::get_singleton()->agent_set_map(get_rid(), agent_parent->get_world_2d()->get_navigation_map());
-				NavigationServer2D::get_singleton()->agent_set_callback(agent, this, "_avoidance_done");
+				set_avoidance_enabled(avoidance_enabled);
 			}
 			set_physics_process_internal(true);
 		} break;
@@ -148,6 +152,19 @@ NavigationAgent2D::NavigationAgent2D() {
 NavigationAgent2D::~NavigationAgent2D() {
 	NavigationServer2D::get_singleton()->free(agent);
 	agent = RID(); // Pointless
+}
+
+void NavigationAgent2D::set_avoidance_enabled(bool p_enabled) {
+	avoidance_enabled = p_enabled;
+	if (avoidance_enabled) {
+		NavigationServer2D::get_singleton()->agent_set_callback(agent, this, "_avoidance_done");
+	} else {
+		NavigationServer2D::get_singleton()->agent_set_callback(agent, nullptr, "_avoidance_done");
+	}
+}
+
+bool NavigationAgent2D::get_avoidance_enabled() const {
+	return avoidance_enabled;
 }
 
 void NavigationAgent2D::set_navigable_layers(uint32_t p_layers) {
@@ -268,7 +285,7 @@ TypedArray<String> NavigationAgent2D::get_configuration_warnings() const {
 	TypedArray<String> warnings = Node::get_configuration_warnings();
 
 	if (!Object::cast_to<Node2D>(get_parent())) {
-		warnings.push_back(RTR("The NavigationAgent2D can be used only under a Node2D node."));
+		warnings.push_back(RTR("The NavigationAgent2D can be used only under a Node2D inheriting parent node."));
 	}
 
 	return warnings;

Relintai/pandemonium_engine

(cherry picked from commit 7f3688603cb3662e425cb7a1dce15459162c1dc6)

scene/2d/navigation_agent_2d.cpp
@@ -38,6 +38,9 @@
 void NavigationAgent2D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_rid"), &NavigationAgent2D::get_rid);
 
+	ClassDB::bind_method(D_METHOD("set_avoidance_enabled", "enabled"), &NavigationAgent2D::set_avoidance_enabled);
+	ClassDB::bind_method(D_METHOD("get_avoidance_enabled"), &NavigationAgent2D::get_avoidance_enabled);
+
 	ClassDB::bind_method(D_METHOD("set_target_desired_distance", "desired_distance"), &NavigationAgent2D::set_target_desired_distance);
 	ClassDB::bind_method(D_METHOD("get_target_desired_distance"), &NavigationAgent2D::get_target_desired_distance);
 
@@ -83,6 +86,7 @@ void NavigationAgent2D::_bind_methods() {
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "time_horizon", PROPERTY_HINT_RANGE, "0.1,10000,0.01"), "set_time_horizon", "get_time_horizon");
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "max_speed", PROPERTY_HINT_RANGE, "0.1,100000,0.01"), "set_max_speed", "get_max_speed");
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "path_max_distance", PROPERTY_HINT_RANGE, "10,100,1"), "set_path_max_distance", "get_path_max_distance");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "avoidance_enabled"), "set_avoidance_enabled", "get_avoidance_enabled");
 
 	ADD_SIGNAL(MethodInfo("path_changed"));
 	ADD_SIGNAL(MethodInfo("target_reached"));
@@ -95,7 +99,7 @@ void NavigationAgent2D::_notification(int p_what) {
 		case NOTIFICATION_READY: {
 			agent_parent = Object::cast_to<Node2D>(get_parent());
 
-			Navigation2DServer::get_singleton()->agent_set_callback(agent, this, "_avoidance_done");
+			set_avoidance_enabled(avoidance_enabled);
 
 			// Search the navigation node and set it
 			{
@@ -154,6 +158,7 @@ NavigationAgent2D::NavigationAgent2D() :
 		agent_parent(nullptr),
 		navigation(nullptr),
 		agent(RID()),
+		avoidance_enabled(false),
 		target_desired_distance(1.0),
 		path_max_distance(3.0),
 		velocity_submitted(false),
@@ -170,6 +175,19 @@ NavigationAgent2D::NavigationAgent2D() :
 NavigationAgent2D::~NavigationAgent2D() {
 	Navigation2DServer::get_singleton()->free(agent);
 	agent = RID(); // Pointless
+}
+
+void NavigationAgent2D::set_avoidance_enabled(bool p_enabled) {
+	avoidance_enabled = p_enabled;
+	if (avoidance_enabled) {
+		Navigation2DServer::get_singleton()->agent_set_callback(agent, this, "_avoidance_done");
+	} else {
+		Navigation2DServer::get_singleton()->agent_set_callback(agent, nullptr, "_avoidance_done");
+	}
+}
+
+bool NavigationAgent2D::get_avoidance_enabled() const {
+	return avoidance_enabled;
 }
 
 void NavigationAgent2D::set_navigation(Navigation2D *p_nav) {
@@ -298,7 +316,7 @@ void NavigationAgent2D::_avoidance_done(Vector3 p_new_velocity) {
 
 String NavigationAgent2D::get_configuration_warning() const {
 	if (!Object::cast_to<Node2D>(get_parent())) {
-		return TTR("The NavigationAgent2D can be used only under a Node2D node.");
+		return TTR("The NavigationAgent2D can be used only under a Node2D inheriting parent node.");
 	}
 
 	return String();