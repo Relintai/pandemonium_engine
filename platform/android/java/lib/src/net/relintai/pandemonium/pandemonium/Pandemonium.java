02eef8fc579cfb885dc592e94df696b4785def18
Relintai <relintai@protonmail.com>
2022-07-27 14:28:26 +0200

    Ported: Check for null when retrieving clip data item text on Android - madmiraal https://github.com/godotengine/godot/commit/fe60de2c6af6617b51a104a770b3e119826563f1

--- platform/android/java/lib/src/net/relintai/pandemonium/pandemonium/Pandemonium.java
+++ platform/android/java/lib/src/net/relintai/pandemonium/pandemonium/Pandemonium.java
@@ -792,14 +792,13 @@ public class Pandemonium extends Fragment implements SensorEventListener, IDownl
 	}
 
 	public String getClipboard() {
-		String copiedText = "";
-
-		if (mClipboard.hasPrimaryClip()) {
-			ClipData.Item item = mClipboard.getPrimaryClip().getItemAt(0);
-			copiedText = item.getText().toString();
-		}
-
-		return copiedText;
+    ClipData clipData = mClipboard.getPrimaryClip();
+		if (clipData == null)
+			return "";
+		CharSequence text = clipData.getItemAt(0).getText();
+		if (text == null)
+			return "";
+		return text.toString();
 	}
 
 	public void setClipboard(String p_text) {
