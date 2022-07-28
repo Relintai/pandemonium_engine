KleirRampage45/redot-engine

Fix `EditorSpinSlider` mouse reset position on tree exit

editor/editor_spin_slider.cpp
@@ -448,6 +448,7 @@ void EditorSpinSlider::_notification(int p_what) {
 			if (grabbing_spinner) {
 				grabber->hide();
 				Input::get_singleton()->set_mouse_mode(Input::MOUSE_MODE_VISIBLE);
+				Input::get_singleton()->warp_mouse(grabbing_spinner_mouse_pos);
 				grabbing_spinner = false;
 				grabbing_spinner_attempt = false;
 			}

Relintai/pandemonium_engine

(cherry picked from commit 784b1adb188f6003f01201d8cff3dab574c6beab)

editor/editor_spin_slider.cpp
@@ -420,6 +420,7 @@ void EditorSpinSlider::_notification(int p_what) {
 			if (grabbing_spinner) {
 				grabber->hide();
 				Input::get_singleton()->set_mouse_mode(Input::MOUSE_MODE_VISIBLE);
+				Input::get_singleton()->warp_mouse_position(grabbing_spinner_mouse_pos);
 				grabbing_spinner = false;
 				grabbing_spinner_attempt = false;
 			}