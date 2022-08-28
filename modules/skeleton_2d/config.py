546929e85b0e526f711e34d2babaef1567a8fb15
Relintai <relintai@protonmail.com>
2022-08-28 14:03:14 +0200

    Add all doc classes to the new module's config.py.

--- modules/skeleton_2d/config.py
+++ modules/skeleton_2d/config.py
@@ -12,6 +12,17 @@ def get_doc_classes():
     return [
         "Skeleton2D",
         "PhysicalBone2D",
+
+        "SkeletonModificationStack2D",
+        
+        "SkeletonModification2D",
+        "SkeletonModification2DCCDIK",
+        "SkeletonModification2DFABRIK",
+        "SkeletonModification2DJiggle",
+        "SkeletonModification2DLookAt",
+        "SkeletonModification2DPhysicalBones",
+        "SkeletonModification2DStackHolder",
+        "SkeletonModification2DTwoBoneIK",
     ]
 
 def get_doc_path():
