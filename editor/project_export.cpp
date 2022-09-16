KleirRampage45/redot-engine

Allow exporting PCK files without export templates installed

editor/export/project_export.cpp
@@ -219,6 +219,7 @@ void ProjectExportDialog::_edit_preset(int p_index) {
 	export_path->show();
 	duplicate_preset->set_disabled(false);
 	delete_preset->set_disabled(false);
+	get_ok_button()->set_disabled(false);
 	name->set_text(current->get_name());
 
 	List<String> extension_list = current->get_platform()->get_binary_extensions(current);
@@ -265,7 +266,6 @@ void ProjectExportDialog::_edit_preset(int p_index) {
 
 		export_warning->hide();
 		export_button->set_disabled(true);
-		get_ok_button()->set_disabled(true);
 	} else {
 		if (error != String()) {
 			Vector<String> items = error.split("\n", false);
@@ -285,7 +285,6 @@ void ProjectExportDialog::_edit_preset(int p_index) {
 		export_error->hide();
 		export_templates_error->hide();
 		export_button->set_disabled(false);
-		get_ok_button()->set_disabled(false);
 	}
 
 	custom_features->set_text(current->get_custom_features());

Relintai/pandemonium_engine

(cherry picked from commit 8d81ce9561748b9a231af70323ddffd288e189ab)

editor/project_export.cpp
@@ -238,6 +238,7 @@ void ProjectExportDialog::_edit_preset(int p_index) {
 	export_path->show();
 	duplicate_preset->set_disabled(false);
 	delete_preset->set_disabled(false);
+	get_ok()->set_disabled(false);
 	name->set_text(current->get_name());
 
 	List<String> extension_list = current->get_platform()->get_binary_extensions(current);
@@ -283,8 +284,6 @@ void ProjectExportDialog::_edit_preset(int p_index) {
 		}
 
 		export_button->set_disabled(true);
-		get_ok()->set_disabled(true);
-
 	} else {
 		if (error != String()) {
 			Vector<String> items = error.split("\n", false);
@@ -303,7 +302,6 @@ void ProjectExportDialog::_edit_preset(int p_index) {
 		export_error->hide();
 		export_templates_error->hide();
 		export_button->set_disabled(false);
-		get_ok()->set_disabled(false);
 	}
 
 	custom_features->set_text(current->get_custom_features());