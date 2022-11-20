07299fdbf6ed6d62b9bce052d0bfcf5c6fc018ef
Relintai <relintai@protonmail.com>
2022-11-20 20:07:31 +0100

    Removed the button properties from PaintProject.

--- modules/paint/nodes/paint_project.cpp
+++ modules/paint/nodes/paint_project.cpp
@@ -124,7 +124,6 @@ void PaintProject::_bind_methods() {
 	ADD_PROPERTY(PropertyInfo(Variant::STRING, "save_file_name"), "set_save_file_name", "get_save_file_name");
 
 	ClassDB::bind_method(D_METHOD("save_image_to_file"), &PaintProject::save_image_to_file);
-	ADD_PROPERTY(PropertyInfo(Variant::NIL, "run_save_image", PROPERTY_HINT_BUTTON, "save_image_to_file"), "", "");
 
 	ClassDB::bind_method(D_METHOD("get_current_color"), &PaintProject::get_current_color);
 	ClassDB::bind_method(D_METHOD("set_current_color", "size"), &PaintProject::set_current_color);
@@ -141,10 +140,7 @@ void PaintProject::_bind_methods() {
 	ADD_PROPERTY(PropertyInfo(Variant::POOL_COLOR_ARRAY, "color_presets"), "set_color_presets", "get_color_presets");
 
 	ClassDB::bind_method(D_METHOD("set_colors_as_default"), &PaintProject::set_colors_as_default);
-	ADD_PROPERTY(PropertyInfo(Variant::NIL, "run_set_colors_as_default", PROPERTY_HINT_BUTTON, "set_colors_as_default"), "", "");
 
 	ClassDB::bind_method(D_METHOD("add_paint_canvas_backgorund"), &PaintProject::add_paint_canvas_backgorund);
 	ClassDB::bind_method(D_METHOD("add_paint_visual_grid"), &PaintProject::add_paint_visual_grid);
-	ADD_PROPERTY(PropertyInfo(Variant::NIL, "run_add_paint_visual_grid", PROPERTY_HINT_BUTTON, "add_paint_visual_grid"), "", "");
-	ADD_PROPERTY(PropertyInfo(Variant::NIL, "run_add_paint_canvas_backgorund", PROPERTY_HINT_BUTTON, "add_paint_canvas_backgorund"), "", "");
 }
