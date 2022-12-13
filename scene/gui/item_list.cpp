e75af6af1d761c7ee66b8b842bc18590e2390ee0
Haoyu Qiu <timothyqiu32@gmail.com>
2022-12-13 15:38:33 +0800

    Fix crash after executing ItemList.set_icon_scale
    (cherry picked from commit 8d0fa193c344be8e1ec801b1a45341801b80a5f8)

--- scene/gui/item_list.cpp
+++ scene/gui/item_list.cpp
@@ -1275,6 +1275,7 @@ bool ItemList::get_allow_reselect() const {
 }
 
 void ItemList::set_icon_scale(real_t p_scale) {
+	ERR_FAIL_COND(Math::is_nan(p_scale) || Math::is_inf(p_scale));
 	icon_scale = p_scale;
 }
 
