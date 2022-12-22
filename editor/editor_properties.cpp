1d009e3fe0fd29789c4c1e374279ebc0c728b6d1
Relintai <relintai@protonmail.com>
2022-12-22 18:54:24 +0100

    Ported:  Fix property link not working in MultiNodeEdit This is due to the `p_field` in the `EditorProperty::emit_changed`. `p_field` only works for `MultiNodeEdit`, not for other objects. - Rindbee and timothyqiu https://github.com/godotengine/godot/commit/9056cc9c2a5a8c7fb8ae0a14b597c8ee36f14ccb

--- editor/editor_properties.cpp
+++ editor/editor_properties.cpp
@@ -1560,7 +1560,7 @@ void EditorPropertyVector2::_value_changed(double val, const String &p_name) {
 	Vector2 v2;
 	v2.x = spin[0]->get_value();
 	v2.y = spin[1]->get_value();
-	emit_changed(get_edited_property(), v2, p_name);
+	emit_changed(get_edited_property(), v2, linked->is_pressed() ? "" : p_name);
 }
 
 void EditorPropertyVector2::update_property() {
@@ -1703,7 +1703,7 @@ void EditorPropertyVector2i::_value_changed(double val, const String &p_name) {
 	Vector2i v2;
 	v2.x = spin[0]->get_value();
 	v2.y = spin[1]->get_value();
-	emit_changed(get_edited_property(), v2, p_name);
+	emit_changed(get_edited_property(), v2, linked->is_pressed() ? "" : p_name);
 }
 
 void EditorPropertyVector2i::update_property() {
@@ -2009,7 +2009,7 @@ void EditorPropertyVector3::_value_changed(double val, const String &p_name) {
 	v3.x = spin[0]->get_value();
 	v3.y = spin[1]->get_value();
 	v3.z = spin[2]->get_value();
-	emit_changed(get_edited_property(), v3, p_name);
+	emit_changed(get_edited_property(), v3, linked->is_pressed() ? "" : p_name);
 }
 
 void EditorPropertyVector3::update_property() {
@@ -2172,7 +2172,7 @@ void EditorPropertyVector3i::_value_changed(double val, const String &p_name) {
 	v3.x = spin[0]->get_value();
 	v3.y = spin[1]->get_value();
 	v3.z = spin[2]->get_value();
-	emit_changed(get_edited_property(), v3, p_name);
+	emit_changed(get_edited_property(), v3, linked->is_pressed() ? "" : p_name);
 }
 
 void EditorPropertyVector3i::update_property() {
