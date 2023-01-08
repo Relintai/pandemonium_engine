4b09cb47ab00a2fa63dbab46bc932e3c19309e53
Relintai <relintai@protonmail.com>
2023-01-08 11:34:32 +0100

    Fix typo.

--- editor_modules/gltf/extensions/gltf_document_extension.cpp
+++ editor_modules/gltf/extensions/gltf_document_extension.cpp
@@ -150,8 +150,8 @@ void GLTFDocumentExtension::convert_scene_node(Ref<GLTFState> p_state, Ref<GLTFN
 }
 
 Error GLTFDocumentExtension::export_node(Ref<GLTFState> p_state, Ref<GLTFNode> p_gltf_node, Dictionary &r_dict, Node *p_node) {
-	ERR_FAIL_COND(p_state.is_null(), ERR_INVALID_PARAMETER);
-	ERR_FAIL_COND(p_gltf_node.is_null(), ERR_INVALID_PARAMETER);
+	ERR_FAIL_COND_V(p_state.is_null(), ERR_INVALID_PARAMETER);
+	ERR_FAIL_COND_V(p_gltf_node.is_null(), ERR_INVALID_PARAMETER);
 	ERR_FAIL_NULL_V(p_node, ERR_INVALID_PARAMETER);
 	ScriptInstance *si = get_script_instance();
 	if (!si) {
