f2682927f00fa4d9274a1a20e7cfe965e9254305
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2023-01-09 21:30:42 +0100

    Expose API to force file system sync.

--- platform/javascript/os_javascript.h
+++ platform/javascript/os_javascript.h
@@ -119,6 +119,7 @@ public:
 	bool check_size_force_redraw();
 	bool pwa_needs_update() const { return pwa_is_waiting; }
 	Error pwa_update();
+	void force_fs_sync();
 
 	// Override return type to make writing static callbacks less tedious.
 	static OS_JavaScript *get_singleton();
