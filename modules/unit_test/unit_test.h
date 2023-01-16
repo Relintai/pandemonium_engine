0a79f38843a5ac61280355453747136b5071a8fb
Relintai <relintai@protonmail.com>
2023-01-16 21:49:15 +0100

    Notes.

--- modules/unit_test/unit_test.h
+++ modules/unit_test/unit_test.h
@@ -16,8 +16,34 @@ public:
 	// bool process() -> return true when finished
 	// bool _process()
 
+	//sub_section_start(name)
+	//sub_section_end()
+
+	// test(name); 
+	// testing(name); //maybe?
+
 	// assert(a == "b", "Testing whenther a == b")
 	// assert_equals(a, "b")    ?  -> could generate text automatically
+
+	//---
+	/*
+	
+	bool _process() {
+		testing("String int conversion.");
+		assert(String::num(1) == "1");
+
+
+		//or
+		testing("String int conversion.");
+		//cpp macro
+		UTASSERT(String::num(1) == "1"); -> assert(String::num(1) == "1", __LINE__);
+
+		sub_section_start(name)
+
+	}
+
+	*/
+	///---
 
 	// process type: process, physics process
 
@@ -26,6 +52,8 @@ public:
 	//ajutomatically count tests, and some data (process time, physics and normal iteration nums)
 
 	//get_runner() -> returns runner
+
+
 
 	UnitTest();
 	virtual ~UnitTest();
