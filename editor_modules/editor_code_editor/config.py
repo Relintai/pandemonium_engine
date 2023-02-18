9490e255c76bc94eb8dcf423d227f69ae5ce95c0
Relintai <relintai@protonmail.com>
2023-02-18 16:54:42 +0100

    Move EditorScriptEditor's doc.

--- editor_modules/editor_code_editor/config.py
+++ editor_modules/editor_code_editor/config.py
@@ -13,8 +13,9 @@ def configure(env):
 
 
 def get_doc_classes():
-    return [
-    ]
+  return [
+    "EditorScriptEditor"
+  ]
 
 def get_doc_path():
-    return "doc_classes"
+  return "doc_classes"
