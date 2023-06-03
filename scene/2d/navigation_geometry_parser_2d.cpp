8cbb1bb879ea478b21c2c90be932009503043202
Relintai <relintai@protonmail.com>
2023-06-03 18:22:12 +0200

    Started porting pull request from godot 4: Rework NavigationMeshGenerator Reworks NavigationMeshGenerator and navigation mesh parse and bake process. Adds navigation mesh baking for 2D. - smix8 https://github.com/godotengine/godot/pull/70724 https://github.com/godotengine/godot/pull/70724/commits/38699a82593ef60d680b7b46b06c92f896dd7c77

---
+++ scene/2d/navigation_geometry_parser_2d.cpp
@@ -0,0 +1,66 @@
+/**************************************************************************/
+/*  navigation_geometry_parser_2d.cpp                                     */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#include "navigation_geometry_parser_2d.h"
+
+#include "scene/2d/node_2d.h"
+
+#include "scene/resources/navigation_mesh_source_geometry_data_2d.h"
+#include "scene/resources/navigation_polygon.h"
+
+bool NavigationGeometryParser2D::parses_node(Node *p_node) {
+	if (Object::cast_to<Node2D>(p_node)) {
+		return false;
+	}
+
+	if (has_method("_parses_node")) {
+		return call("_parses_node", p_node);
+	}
+
+	return false;
+}
+
+void NavigationGeometryParser2D::parse_node_geometry(Ref<NavigationPolygon> p_navigation_polygon, Node *p_node, Ref<NavigationMeshSourceGeometryData2D> p_source_geometry) {
+	parse_geometry(p_node, p_navigation_polygon, p_source_geometry);
+}
+
+void NavigationGeometryParser2D::parse_geometry(Node *p_node, Ref<NavigationPolygon> p_navigation_polygon, Ref<NavigationMeshSourceGeometryData2D> p_source_geometry) {
+	if (has_method("_parse_geometry")) {
+		call("_parse_geometry", p_node, p_navigation_polygon, p_source_geometry);
+	}
+}
+
+void NavigationGeometryParser2D::_bind_methods() {
+	BIND_VMETHOD(MethodInfo(Variant::BOOL, "_parses_node", PropertyInfo(Variant::OBJECT, "node", PROPERTY_HINT_RESOURCE_TYPE, "Node")));
+	BIND_VMETHOD(MethodInfo("_parse_geometry",
+			PropertyInfo(Variant::OBJECT, "node", PROPERTY_HINT_RESOURCE_TYPE, "Node"),
+			PropertyInfo(Variant::OBJECT, "navigation_polygon", PROPERTY_HINT_RESOURCE_TYPE, "NavigationPolygon"),
+			PropertyInfo(Variant::OBJECT, "source_geometry", PROPERTY_HINT_RESOURCE_TYPE, "NavigationMeshSourceGeometryData2D")));
+}
---
+++ scene/resources/navigation_mesh_source_geometry_data_3d.h
@@ -0,0 +1,79 @@
+#ifndef NAVIGATION_MESH_SOURCE_GEOMETRY_DATA_3D_H
+#define NAVIGATION_MESH_SOURCE_GEOMETRY_DATA_3D_H
+
+/**************************************************************************/
+/*  navigation_mesh_source_geometry_data_3d.h                             */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#include "core/object/resource.h"
+
+#include "core/math/transform.h"
+
+#include "mesh.h"
+
+class NavigationMeshSourceGeometryData3D : public Resource {
+	GDCLASS(NavigationMeshSourceGeometryData3D, Resource);
+
+	PoolRealArray vertices;
+	PoolIntArray indices;
+
+protected:
+	static void _bind_methods();
+
+private:
+	void _add_vertex(const Vector3 &p_vec3);
+	void _add_mesh(const Ref<Mesh> &p_mesh, const Transform &p_xform);
+	void _add_mesh_array(const Array &p_array, const Transform &p_xform);
+	void _add_faces(const PoolVector3Array &p_faces, const Transform &p_xform);
+
+public:
+	// kept root node transform here on the geometry data
+	// if we add this transform to all exposed functions we need to break comp on all functions later
+	// when navmesh changes from global transfrom to relative to navregion
+	// but if it stays here we can just remove it and change the internal functions only
+	Transform root_node_transform;
+
+	void set_vertices(const PoolRealArray &p_vertices);
+	PoolRealArray get_vertices() const { return vertices; }
+
+	void set_indices(const PoolIntArray &p_indices);
+	PoolIntArray get_indices() const { return indices; }
+
+	bool has_data() { return vertices.size() && indices.size(); };
+	void clear();
+
+	void add_mesh(const Ref<Mesh> &p_mesh, const Transform &p_xform);
+	void add_mesh_array(const Array &p_mesh_array, const Transform &p_xform);
+	void add_faces(const PoolVector3Array &p_faces, const Transform &p_xform);
+
+	NavigationMeshSourceGeometryData3D();
+	~NavigationMeshSourceGeometryData3D();
+};
+
+#endif // NAVIGATION_MESH_SOURCE_GEOMETRY_DATA_3D_H
