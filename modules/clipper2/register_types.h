6f88656bc7496b89b00708becc4fa59e659eb2d7
Relintai <relintai@protonmail.com>
2023-06-07 15:33:59 +0200

    Removed the Clipper2 module, and also the remaining c++17 defines.

--- modules/clipper2/register_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLIPPER2_REGISTER_TYPES_H
-#define CLIPPER2_REGISTER_TYPES_H
-
-#include "modules/register_module_types.h"
-
-void register_clipper2_types(ModuleRegistrationLevel p_level);
-void unregister_clipper2_types(ModuleRegistrationLevel p_level);
-
-#endif
