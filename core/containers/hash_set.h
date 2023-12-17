c12e2b34adc07114702e595128ac16da52f9c2a3
Relintai <relintai@protonmail.com>
2023-12-17 22:39:31 +0100

    File copyright header updates pt2.

--- core/containers/hash_set.h
+++ core/containers/hash_set.h
@@ -1,12 +1,16 @@
+#ifndef HASH_SET_H
+#define HASH_SET_H
+
 /*************************************************************************/
 /*  hash_set.h                                                           */
 /*************************************************************************/
-/*                       This file is part of:                           */
-/*                           GODOT ENGINE                                */
-/*                      https://godotengine.org                          */
+/*                         This file is part of:                         */
+/*                          PANDEMONIUM ENGINE                           */
+/*             https://github.com/Relintai/pandemonium_engine            */
 /*************************************************************************/
-/* Copyright (c) 2007-2022 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2022-present Péter Magyar.                              */
 /* Copyright (c) 2014-2022 Godot Engine contributors (cf. AUTHORS.md).   */
+/* Copyright (c) 2007-2022 Juan Linietsky, Ariel Manzur.                 */
 /*                                                                       */
 /* Permission is hereby granted, free of charge, to any person obtaining */
 /* a copy of this software and associated documentation files (the       */
@@ -28,8 +32,6 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#ifndef HASH_SET_H
-#define HASH_SET_H
 
 #include "core/containers/hash_map.h"
 #include "core/containers/hashfuncs.h"
