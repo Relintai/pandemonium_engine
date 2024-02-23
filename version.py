b3cf4d9dc252bab195aef76a303b18163a92056e
Relintai <relintai@protonmail.com>
2024-02-23 13:07:25 +0100

    Set version to 4.2.1 stable.

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 short_name = "pandemonium_engine"
 name = "Pandemonium Engine"
 major = 4
-minor = 3
-patch = 0
-status = "development"
+minor = 2
+patch = 1
+status = "stable"
 module_config = ""
 year = 2024
 website = "https://github.com/Relintai/pandemonium_engine"
