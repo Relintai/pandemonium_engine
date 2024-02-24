4b9982d2d95da4268c1f45a136990b275baa9b49
Relintai <relintai@protonmail.com>
2024-02-24 10:33:52 +0100

    Bump version.

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 short_name = "pandemonium_engine"
 name = "Pandemonium Engine"
 major = 4
-minor = 2
-patch = 1
-status = "stable"
+minor = 3
+patch = 0
+status = "development"
 module_config = ""
 year = 2024
 website = "https://github.com/Relintai/pandemonium_engine"
