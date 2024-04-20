836e4a4f3876a5176b2d927569c07fa9ba217b48
Relintai <relintai@protonmail.com>
2024-04-20 21:23:49 +0200

    Remove podman check from the build all script.

--- misc/build-containers/podman_build_all.sh
+++ misc/build-containers/podman_build_all.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-podman=`which podman || true`
-
-if [ -z $podman ]; then
-  echo "podman needs to be in PATH for this script to work."
-  exit 1
-fi
-
 basedir=$(pwd)/
 
 cd ../..
