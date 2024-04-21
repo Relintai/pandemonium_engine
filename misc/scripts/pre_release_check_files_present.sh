de192f0f2bb8e3b7cc14b76767d204d626857eba
Relintai <relintai@protonmail.com>
2024-04-21 21:34:09 +0200

    Update the pre release file script's file list.

--- misc/scripts/pre_release_check_files_present.sh
+++ misc/scripts/pre_release_check_files_present.sh
@@ -1,35 +1,58 @@
 cd ../../bin/
 
 files=(
-  # Windows (64 bit)
+  # Windows
   "pandemonium.windows.opt.64.exe"
   "pandemonium.windows.opt.debug.64.exe"
-  "pandemonium.windows.opt.tools.64.exe"
-
-  # Windows (32 bit)
+  
   "pandemonium.windows.opt.32.exe"
   "pandemonium.windows.opt.debug.32.exe"
+
+  "pandemonium.windows.opt.tools.64.exe"
   "pandemonium.windows.opt.tools.32.exe"
-  
-  # Linux (64 bit)
+
+  # Linux
+  "pandemonium.x11.opt.32"
   "pandemonium.x11.opt.64"
+  "pandemonium.x11.opt.arm"
+  "pandemonium.x11.opt.arm64"
+
+  "pandemonium.x11.opt.debug.32"
   "pandemonium.x11.opt.debug.64"
+  "pandemonium.x11.opt.debug.arm"
+  "pandemonium.x11.opt.debug.arm64"
+
+  "pandemonium.x11.opt.tools.32"
   "pandemonium.x11.opt.tools.64"
+  "pandemonium.x11.opt.tools.arm"
+  "pandemonium.x11.opt.tools.arm64"
 
-  # Pi4
-  "pandemonium.x11.opt.pi4"
-  "pandemonium.x11.opt.debug.pi4"
-  "pandemonium.x11.opt.tools.pi4"
-
-  # Server - Linux (64 bit)
+  # Server (Linux)
+  "pandemonium_server.x11.opt.32"
   "pandemonium_server.x11.opt.64"
-  "pandemonium_server.x11.opt.debug.64"
-  "pandemonium_server.x11.opt.tools.64"
+  "pandemonium_server.x11.opt.arm"
+  "pandemonium_server.x11.opt.arm64"
 
-  # Linux (32 bit)
-  "pandemonium.x11.opt.64"
-  "pandemonium.x11.opt.debug.64"
-  "pandemonium.x11.opt.tools.64"
+  "pandemonium_server.x11.opt.debug.32"
+  "pandemonium_server.x11.opt.debug.64"
+  "pandemonium_server.x11.opt.debug.arm"
+  "pandemonium_server.x11.opt.debug.arm64"
+
+  "pandemonium_server.x11.opt.tools.32"
+  "pandemonium_server.x11.opt.tools.64"
+  "pandemonium_server.x11.opt.tools.arm"
+  "pandemonium_server.x11.opt.tools.arm64"
+
+  # HTTP Server (Linux)
+  "pandemonium_http_server.x11.opt.32"
+  "pandemonium_http_server.x11.opt.64"
+  "pandemonium_http_server.x11.opt.arm"
+  "pandemonium_http_server.x11.opt.arm64"
+
+  "pandemonium_http_server.x11.opt.debug.32"
+  "pandemonium_http_server.x11.opt.debug.64"
+  "pandemonium_http_server.x11.opt.debug.arm"
+  "pandemonium_http_server.x11.opt.debug.arm64"
 
   # JS
   "pandemonium.javascript.opt.zip"
@@ -40,12 +63,46 @@ files=(
 
   "pandemonium.javascript.opt.tools.threads.zip"
 
+  "pandemonium.javascript.opt.gdnative.zip"
+  "pandemonium.javascript.opt.debug.gdnative.zip"
+
   # Android
   "android_source.zip"
   "android_debug.apk"
   "android_release.apk"
   "android_editor.apk"
   "pandemonium-lib.release.aar"
+  "pandemonium-lib.debug.aar"
+
+  # OSX
+  "pandemonium.osx.opt.universal"
+  "pandemonium.osx.opt.debug.universal"
+  "pandemonium.osx.opt.tools.universal"
+
+  # iOS
+  "libpandemonium.iphone.opt.arm64.a"
+  "libpandemonium.iphone.opt.debug.arm64.a"
+
+  "libpandemonium.iphone.opt.debug.x86_64.simulator.a"
+  "libpandemonium.iphone.opt.x86_64.simulator.a"
+
+  # FRT
+  "pandemonium.frt.opt.arm32v6"
+  "pandemonium.frt.opt.arm32v7"
+  "pandemonium.frt.opt.arm64v8"
+
+  "pandemonium.frt.opt.debug.arm32v6"
+  "pandemonium.frt.opt.debug.arm32v7"
+  "pandemonium.frt.opt.debug.arm64v8"
+
+  # FRT SDL
+  "pandemonium.frt_sdl.opt.arm32v6"
+  "pandemonium.frt_sdl.opt.arm32v7"
+  "pandemonium.frt_sdl.opt.arm64v8"
+
+  "pandemonium.frt_sdl.opt.debug.arm32v6"
+  "pandemonium.frt_sdl.opt.debug.arm32v7"
+  "pandemonium.frt_sdl.opt.debug.arm64v8"
 
   # OSX - Editor
   "Pandemonium.app.zip"
