11b3b21ab6578e77150f23bd3864377ce321b25f
Relintai <relintai@protonmail.com>
2024-04-21 11:58:01 +0200

    frt in-container build script cleanups.

--- misc/build-containers/engine_build_scripts/frt_arm64.sh
+++ misc/build-containers/engine_build_scripts/frt_arm64.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-# production=yes
-
 # FRT templates arm64v8
-scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=frt frt_arch=arm64v8 frt_cross=auto "$@"
-scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=frt frt_arch=arm64v8 frt_cross=auto "$@"
+scons production=yes tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=frt frt_arch=arm64v8 frt_cross=auto "$@"
+scons production=yes tools=no target=release custom_modules_shared=no debug_symbols=no platform=frt frt_arch=arm64v8 frt_cross=auto "$@"
 
 # FRT SDL templates arm64v8
-scons tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=frt_sdl frt_arch=arm64v8 frt_cross=auto "$@"
-scons tools=no target=release custom_modules_shared=no debug_symbols=no platform=frt_sdl frt_arch=arm64v8 frt_cross=auto "$@"
+scons production=yes tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=frt_sdl frt_arch=arm64v8 frt_cross=auto "$@"
+scons production=yes tools=no target=release custom_modules_shared=no debug_symbols=no platform=frt_sdl frt_arch=arm64v8 frt_cross=auto "$@"
 
