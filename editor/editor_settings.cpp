f3771810350f6c931f941700a9601c82bd9e1fb9
Relintai <relintai@protonmail.com>
2024-07-18 08:14:27 +0200

    Tweaks to defaults.

--- editor/editor_settings.cpp
+++ editor/editor_settings.cpp
@@ -518,7 +518,7 @@ void EditorSettings::_load_defaults(Ref<ConfigFile> p_extra_config) {
 	_initial_set("text_editor/completion/put_callhint_tooltip_below_current_line", true);
 	_initial_set("text_editor/completion/callhint_tooltip_offset", Vector2());
 	_initial_set("text_editor/completion/complete_file_paths", true);
-	_initial_set("text_editor/completion/add_type_hints", false);
+	_initial_set("text_editor/completion/add_type_hints", true);
 	_initial_set("text_editor/completion/use_single_quotes", false);
 
 	// Help
