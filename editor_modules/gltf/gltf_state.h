KleirRampage45/redot-engine

GLTF: Fixed external images getting embedded on import
Added a map to keep track of external images during import, and used
that map to instance the textures using the resource loader instead of
creating a new texture from scratch

modules/gltf/gltf_state.h
@@ -78,6 +78,7 @@ class GLTFState : public Resource {
 	Vector<Ref<GLTFTextureSampler>> texture_samplers;
 	Ref<GLTFTextureSampler> default_texture_sampler;
 	Vector<Ref<Image>> images;
+	Map<GLTFImageIndex, String> external_images_paths;
 	Map<GLTFTextureIndex, Ref<Texture>> texture_cache;
 	Vector<String> extensions_used;
 	Vector<String> extensions_required;

Relintai/pandemonium_engine

editor_modules/gltf/gltf_state.h
@@ -79,6 +79,7 @@ class GLTFState : public Resource {
 	Vector<Ref<GLTFTextureSampler>> texture_samplers;
 	Ref<GLTFTextureSampler> default_texture_sampler;
 	Vector<Ref<Image>> images;
+	RBMap<GLTFImageIndex, String> external_images_paths;
 	RBMap<GLTFTextureIndex, Ref<Texture>> texture_cache;
 	Vector<String> extensions_used;
 	Vector<String> extensions_required;