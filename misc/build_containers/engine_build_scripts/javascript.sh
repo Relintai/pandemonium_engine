99ef4fcdd21869de3632df42ad41468d0747488f
Relintai <relintai@relintai.net>
2025-06-23 23:38:55 +0200

    Don't build the javascript gdnative + threads editor via the build scripts.

--- misc/build_containers/engine_build_scripts/javascript.sh
+++ misc/build_containers/engine_build_scripts/javascript.sh
@@ -3,12 +3,8 @@ set -e
 
 source /root/emsdk/emsdk_env.sh
 
-
 # Javascript editor
 scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no threads_enabled=yes platform=javascript "$@"
-
-# Javascript editor with GDNative
-scons tools=yes target=release_debug custom_modules_shared=no debug_symbols=no gdnative_enabled=yes threads_enabled=yes platform=javascript "$@"
 
 # Javascript templates normal
 scons production=yes tools=no target=release_debug custom_modules_shared=no debug_symbols=no platform=javascript "$@"
