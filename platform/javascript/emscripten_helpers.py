5f3987a0a433f00a4d6b333349ba99873ef45c6c
Relintai <relintai@relintai.net>
2025-06-23 09:18:02 +0200

    Fix javacript build.
    Why this used to work at all? That logic was definitely flawed, and for
    a long time. Did something change in scons? TODO.

--- platform/javascript/emscripten_helpers.py
+++ platform/javascript/emscripten_helpers.py
@@ -54,9 +54,9 @@ def create_template_zip(env, js, wasm, extra):
     if env["gdnative_enabled"]:
         in_files.append(extra)  # Runtime
         out_files.append(zip_dir.File(binary_name + ".side.wasm"))
-    elif env["threads_enabled"]:
-        in_files.append(extra)  # Worker
-        out_files.append(zip_dir.File(binary_name + ".worker.js"))
+    #elif env["threads_enabled"]:
+    #   in_files.append(extra)  # Worker
+    #   out_files.append(zip_dir.File(binary_name + ".worker.js"))
 
     service_worker = "#misc/dist/html/service-worker.js"
     if env["tools"]:
