24f0bdfb786f7e221d9ce4e2c0a78e49e2d16f93
Relintai <relintai@relintai.net>
2025-08-06 21:33:17 +0200

    Added the skeleton_3d module as a hard dependency for the fbx module.

--- editor_modules/fbx/config.py
+++ editor_modules/fbx/config.py
@@ -1,16 +1,17 @@
-def can_build(env, platform):
-    return env["tools"]
 
+def can_build(env, platform):
+    env.module_add_dependencies("fbx", ["skeleton_3d"], False)
+
+    return env["tools"]
 
 def configure(env):
     pass
-
 
 def get_doc_classes():
     return [
         "EditorSceneImporterFBX",
     ]
 
-
 def get_doc_path():
     return "doc_classes"
+
