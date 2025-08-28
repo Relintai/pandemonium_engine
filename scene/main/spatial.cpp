f09e034859e338b67fd4887cbf7b3aa0c27e72fe
lawnjelly <lawnjelly@gmail.com>
2025-08-28 18:08:00 +0100

    Fix `is_visible_in_tree` regression for out of tree

--- scene/main/spatial.cpp
+++ scene/main/spatial.cpp
@@ -960,14 +960,15 @@ void Spatial::show() {
 
 	data.visible = true;
 
-	if (!is_inside_tree()) {
-		return;
-	}
-
 	bool parent_visible = get_parent_spatial() ? get_parent_spatial()->data.visible_in_tree : true;
 	if (parent_visible) {
 		_propagate_visible_in_tree(true);
 	}
+
+	if (!is_inside_tree()) {
+		return;
+	}
+
 	_propagate_visibility_changed();
 }
 
@@ -978,14 +979,15 @@ void Spatial::hide() {
 
 	data.visible = false;
 
-	if (!is_inside_tree()) {
-		return;
-	}
-
 	bool parent_visible = get_parent_spatial() ? get_parent_spatial()->data.visible_in_tree : true;
 	if (parent_visible) {
 		_propagate_visible_in_tree(false);
 	}
+
+	if (!is_inside_tree()) {
+		return;
+	}
+
 	_propagate_visibility_changed();
 }
 
