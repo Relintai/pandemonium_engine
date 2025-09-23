b5c3697adf7c4d37b8c90a8d7e46fd97c0e163ea
Relintai <relintai@relintai.net>
2025-09-23 07:01:53 +0200

    Bump version.

--- version.py
+++ version.py
@@ -1,10 +1,10 @@
 short_name = "pandemonium_engine"
 name = "Pandemonium Engine"
 major = 4
-minor = 8
+minor = 9
 patch = 0
-status = "stable"
+status = "development"
 module_config = ""
 year = 2025
 website = "https://github.com/Relintai/pandemonium_engine"
-docs = "4.8"
+docs = "master"
