2588cecfc052ea29bf2ff0575df18a21a0375640
Relintai <relintai@relintai.net>
2025-11-11 08:31:04 +0100

    Define slim_args_editor in the sample module_config.py, and use that to fill slim_args instead of the other way around.
    It's more convenient this way, as a slim editor build will usually need
    more modules to be enabled.

--- misc/scripts_app/module_config.py
+++ misc/scripts_app/module_config.py
@@ -41,99 +41,99 @@ third_party_addon_repositories = [
 # as the custom_modules= parameter.
 custom_module_folders = ""
 
-# When building the slim templates ("_slim" build word)
+# When building the slim editor ("_slim" build word)
 # These commands will be passed to the build.
-slim_args = ''
+slim_args_editor = ''
 
 # Normal Modules
 
-#slim_args += 'module_bmp_enabled=no '
-#slim_args += 'module_broken_seals_module_enabled=no '
-#slim_args += 'module_cscript_enabled=no '
-#slim_args += 'module_csg_enabled=no '
-#slim_args += 'module_database_enabled=no '
-#slim_args += 'module_database_sqlite_enabled=no '
-#slim_args += 'module_dds_enabled=no '
-#slim_args += 'module_enet_enabled=no '
-#slim_args += 'module_entity_spell_system_enabled=no '
-#slim_args += 'module_fastnoise_enabled=no ' 
-#slim_args += 'module_freetype_enabled=no ' # Module cannot be disabled with the tools (editor) build
-#slim_args += 'module_gdnative_enabled=no ' 
-#slim_args += 'module_gdscript_enabled=no ' 
-#slim_args += 'module_gridmap_enabled=no '
-#slim_args += 'module_hdr_enabled=no '
-#slim_args += 'module_http_server_simple_enabled=no '
-#slim_args += 'module_jpg_enabled=no '
-#slim_args += 'module_lz4_enabled=no '
-#slim_args += 'module_material_maker_enabled=no '
-#slim_args += 'module_mbedtls_enabled=no '
-#slim_args += 'module_mesh_data_resource_enabled=no '
-#slim_args += 'module_mesh_utils_enabled=no '
-#slim_args += 'module_minimp3_enabled=no '
-#slim_args += 'module_navigation_enabled=no ' # The default (working) navigationserver backend. Currently at least one navigation server backend need to be present, so keep at least one active.
-#slim_args += 'module_navigation_dummy_enabled=no ' # The dummy navigationserver backend.
-#slim_args += 'module_navigation_geometry_parsers_enabled=no '
-#slim_args += 'module_navigation_mesh_generator_enabled=no '
-#slim_args += 'module_network_synchronizer_enabled=no '
-#slim_args += 'module_ogg_enabled=no '
-#slim_args += 'module_opensimplex_enabled=no '
-#slim_args += 'module_opus_enabled=no '
-#slim_args += 'module_paint_enabled=no '
-#slim_args += 'module_procedural_tree_3d_enabled=no '
-#slim_args += 'module_props_enabled=no '
-#slim_args += 'module_props_2d_enabled=no '
-#slim_args += 'module_pvr_enabled=no '
-#slim_args += 'module_regex_enabled=no ' # Module cannot be disabled with the tools (editor) build
-#slim_args += 'module_skeleton_2d_enabled=no '
-#slim_args += 'module_skeleton_3d_enabled=no '
-#slim_args += 'module_smtp_enabled=no '
-#slim_args += 'module_squish_enabled=no '
-#slim_args += 'module_stb_vorbis_enabled=no '
-#slim_args += 'module_steering_ai_enabled=no '
-#slim_args += 'module_svg_enabled=no ' # If module is disabled in editor builds, editor icons won't appear.
-#slim_args += 'module_terraman_enabled=no '
-#slim_args += 'module_terraman_2d_enabled=no '
-#slim_args += 'module_texture_packer_enabled=no '
-#slim_args += 'module_tga_enabled=no '
-#slim_args += 'module_theora_enabled=no '
-#slim_args += 'module_tile_map_enabled=no '
-#slim_args += 'module_ui_extensions_enabled=no '
-#slim_args += 'module_unit_test_enabled=no '
-#slim_args += 'module_upnp_enabled=no '
-#slim_args += 'module_users_enabled=no '
-#slim_args += 'module_vhacd_enabled=no '
-#slim_args += 'module_vorbis_enabled=no '
-#slim_args += 'module_voxelman_enabled=no '
-#slim_args += 'module_web_enabled=no '
-#slim_args += 'module_websocket_enabled=no '
-#slim_args += 'module_wfc_enabled=no '
-#slim_args += 'module_jsonrpc_enabled=no ' # JSONRPC implementation. Currently only used by gdscript's lsp (when it provides autocomplete for an another ide).
-#slim_args += 'module_layered_tile_map_enabled=no '
-#slim_args += 'module_totp_enabled=no '
-#slim_args += 'module_vertex_lights_2d_enabled=no '
-#slim_args += 'module_vertex_lights_3d_enabled=no '
-#slim_args += 'module_xatlas_unwrap_enabled=no ' # GLES3 lihgtmapper
+#slim_args_editor += 'module_bmp_enabled=no '
+#slim_args_editor += 'module_broken_seals_module_enabled=no '
+#slim_args_editor += 'module_cscript_enabled=no '
+#slim_args_editor += 'module_csg_enabled=no '
+#slim_args_editor += 'module_database_enabled=no '
+#slim_args_editor += 'module_database_sqlite_enabled=no '
+#slim_args_editor += 'module_dds_enabled=no '
+#slim_args_editor += 'module_enet_enabled=no '
+#slim_args_editor += 'module_entity_spell_system_enabled=no '
+#slim_args_editor += 'module_fastnoise_enabled=no ' 
+#slim_args_editor += 'module_freetype_enabled=no ' # Module cannot be disabled with the tools (editor) build
+#slim_args_editor += 'module_gdnative_enabled=no ' 
+#slim_args_editor += 'module_gdscript_enabled=no ' 
+#slim_args_editor += 'module_gridmap_enabled=no '
+#slim_args_editor += 'module_hdr_enabled=no '
+#slim_args_editor += 'module_http_server_simple_enabled=no '
+#slim_args_editor += 'module_jpg_enabled=no '
+#slim_args_editor += 'module_lz4_enabled=no '
+#slim_args_editor += 'module_material_maker_enabled=no '
+#slim_args_editor += 'module_mbedtls_enabled=no '
+#slim_args_editor += 'module_mesh_data_resource_enabled=no '
+#slim_args_editor += 'module_mesh_utils_enabled=no '
+#slim_args_editor += 'module_minimp3_enabled=no '
+#slim_args_editor += 'module_navigation_enabled=no ' # The default (working) navigationserver backend. Currently at least one navigation server backend need to be present, so keep at least one active.
+#slim_args_editor += 'module_navigation_dummy_enabled=no ' # The dummy navigationserver backend.
+#slim_args_editor += 'module_navigation_geometry_parsers_enabled=no '
+#slim_args_editor += 'module_navigation_mesh_generator_enabled=no '
+#slim_args_editor += 'module_network_synchronizer_enabled=no '
+#slim_args_editor += 'module_ogg_enabled=no '
+#slim_args_editor += 'module_opensimplex_enabled=no '
+#slim_args_editor += 'module_opus_enabled=no '
+#slim_args_editor += 'module_paint_enabled=no '
+#slim_args_editor += 'module_procedural_tree_3d_enabled=no '
+#slim_args_editor += 'module_props_enabled=no '
+#slim_args_editor += 'module_props_2d_enabled=no '
+#slim_args_editor += 'module_pvr_enabled=no '
+#slim_args_editor += 'module_regex_enabled=no ' # Module cannot be disabled with the tools (editor) build
+#slim_args_editor += 'module_skeleton_2d_enabled=no '
+#slim_args_editor += 'module_skeleton_3d_enabled=no '
+#slim_args_editor += 'module_smtp_enabled=no '
+#slim_args_editor += 'module_squish_enabled=no '
+#slim_args_editor += 'module_stb_vorbis_enabled=no '
+#slim_args_editor += 'module_steering_ai_enabled=no '
+#slim_args_editor += 'module_svg_enabled=no ' # If module is disabled in editor builds, editor icons won't appear.
+#slim_args_editor += 'module_terraman_enabled=no '
+#slim_args_editor += 'module_terraman_2d_enabled=no '
+#slim_args_editor += 'module_texture_packer_enabled=no '
+#slim_args_editor += 'module_tga_enabled=no '
+#slim_args_editor += 'module_theora_enabled=no '
+#slim_args_editor += 'module_tile_map_enabled=no '
+#slim_args_editor += 'module_ui_extensions_enabled=no '
+#slim_args_editor += 'module_unit_test_enabled=no '
+#slim_args_editor += 'module_upnp_enabled=no '
+#slim_args_editor += 'module_users_enabled=no '
+#slim_args_editor += 'module_vhacd_enabled=no '
+#slim_args_editor += 'module_vorbis_enabled=no '
+#slim_args_editor += 'module_voxelman_enabled=no '
+#slim_args_editor += 'module_web_enabled=no '
+#slim_args_editor += 'module_websocket_enabled=no '
+#slim_args_editor += 'module_wfc_enabled=no '
+#slim_args_editor += 'module_jsonrpc_enabled=no ' # JSONRPC implementation. Currently only used by gdscript's lsp (when it provides autocomplete for an another ide).
+#slim_args_editor += 'module_layered_tile_map_enabled=no '
+#slim_args_editor += 'module_totp_enabled=no '
+#slim_args_editor += 'module_vertex_lights_2d_enabled=no '
+#slim_args_editor += 'module_vertex_lights_3d_enabled=no '
+#slim_args_editor += 'module_xatlas_unwrap_enabled=no ' # GLES3 lihgtmapper
 
 # Editor Modules
 
-#slim_args += 'module_cvtt_enabled=no '
-#slim_args += 'module_editor_code_editor_enabled=no '
-#slim_args += 'module_etc_enabled=no '
-#slim_args += 'module_gltf_enabled=no '
-#slim_args += 'module_plugin_refresher_enabled=no '
-#slim_args += 'module_shader_editor_enabled=no '
-#slim_args += 'module_text_editor_enabled=no '
-#slim_args += 'module_tinyexr_enabled=no '
-#slim_args += 'module_denoise_enabled=no ' # GLES3 lihgtmapper
-#slim_args += 'module_lightmapper_cpu_enabled=no ' # GLES3 lihgtmapper 
-#slim_args += 'module_raycast_enabled=no ' # GLES3 lihgtmapper 
-#slim_args += 'module_fbx_enabled=no '
+#slim_args_editor += 'module_cvtt_enabled=no '
+#slim_args_editor += 'module_editor_code_editor_enabled=no '
+#slim_args_editor += 'module_etc_enabled=no '
+#slim_args_editor += 'module_gltf_enabled=no '
+#slim_args_editor += 'module_plugin_refresher_enabled=no '
+#slim_args_editor += 'module_shader_editor_enabled=no '
+#slim_args_editor += 'module_text_editor_enabled=no '
+#slim_args_editor += 'module_tinyexr_enabled=no '
+#slim_args_editor += 'module_denoise_enabled=no ' # GLES3 lihgtmapper
+#slim_args_editor += 'module_lightmapper_cpu_enabled=no ' # GLES3 lihgtmapper 
+#slim_args_editor += 'module_raycast_enabled=no ' # GLES3 lihgtmapper 
+#slim_args_editor += 'module_fbx_enabled=no '
 
-#slim_args += 'disable_advanced_gui=yes ' # Cannot be used with the tools (editor) build
-#slim_args += 'disable_3d=yes ' # Cannot be used with the tools (editor) build
-#slim_args += 'disable_gles3=yes '
+#slim_args_editor += 'disable_advanced_gui=yes ' # Cannot be used with the tools (editor) build
+#slim_args_editor += 'disable_3d=yes ' # Cannot be used with the tools (editor) build
+#slim_args_editor += 'disable_gles3=yes '
 
-# When building the slim editor ("_slim" build word)
+# When building the slim templates ("_slim" build word)
 # These commands will be passed to the build.
-slim_args_editor = slim_args
+slim_args = slim_args_editor
 
