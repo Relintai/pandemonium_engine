e9a5111b2774e8cf5a5881c2d63917a9e79437c2
Relintai <relintai@relintai.net>
2025-12-07 10:35:23 +0100

    Added the camera module from godot 4. I renamed it to camera_drivers.

---
+++ modules/camera_drivers/config.py
@@ -0,0 +1,13 @@
+def can_build(env, platform):
+    # temp
+    return False
+
+    import sys
+
+    if sys.platform.startswith("freebsd") or sys.platform.startswith("openbsd"):
+        return False
+    return platform == "macos" or platform == "windows" or platform == "linuxbsd" or platform == "android"
+
+
+def configure(env):
+    pass
