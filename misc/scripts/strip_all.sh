3f835e9daaca6cc9c746b34634b0973b2568eff9
Relintai <relintai@relintai.net>
2025-12-23 08:00:34 +0100

    Also strip the server and http server variants.

--- misc/scripts/strip_all.sh
+++ misc/scripts/strip_all.sh
@@ -7,3 +7,6 @@ cd ../../bin/
 strip -v -s pandemonium.x11.*
 strip -v -s pandemonium_server.x11.*
 strip -v -s pandemonium.windows.*
+strip -v -s pandemonium_server.*
+strip -v -s pandemonium_http_server.*
+
