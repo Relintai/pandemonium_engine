2b498dace03446f078f365f54d7dc6934cfa4b10
Relintai <relintai@relintai.net>
2025-12-23 08:00:14 +0100

    Use verbose output, and explicitly strip all.

--- misc/scripts/strip_all.sh
+++ misc/scripts/strip_all.sh
@@ -4,6 +4,6 @@ cd ../../bin/
 
 # Todo figure out a way to also strip the pi4 versions (from x86 linux)
 
-strip pandemonium.x11.*
-strip pandemonium_server.x11.*
-strip pandemonium.windows.*
+strip -v -s pandemonium.x11.*
+strip -v -s pandemonium_server.x11.*
+strip -v -s pandemonium.windows.*
