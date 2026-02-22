aa2c2df209289aea1ef122e9b754e94d2401202f
Relintai <relintai@relintai.net>
2026-02-22 15:36:44 +0100

    Also use dispatch semaphore on ios.

--- platform/iphone/platform_semaphore.h
+++ platform/iphone/platform_semaphore.h
@@ -33,65 +33,53 @@
 /*************************************************************************/
 
 #include "core/error/error_list.h"
-#include "core/typedefs.h"
 #include "core/error/error_macros.h"
+#include "core/os/safe_refcount.h"
+#include "core/typedefs.h"
 
+#include <dispatch/dispatch.h>
 #include <errno.h>
-#include <semaphore.h>
 
 class Semaphore {
 public:
 	_ALWAYS_INLINE_ void post() const {
-		sem_post(&_sem);
+		_count.increment();
+		dispatch_semaphore_signal(_sem);
 	}
 
 	_ALWAYS_INLINE_ void wait() const {
-		//return 0 on success; on error, the value of the semaphore is left unchanged, -1 is returned, and errno is set to indicate the error.
-		while (sem_wait(&_sem)) {
-			if (errno == EINTR) {
-				//he call was interrupted by a signal handler;
-				errno = 0;
-				continue;
-			} else {
-				return;
-			}
-		}
+		dispatch_semaphore_wait(_sem, DISPATCH_TIME_FOREVER);
+		_count.decrement();
 	}
 
 	_ALWAYS_INLINE_ bool try_wait() const {
-		while (sem_trywait(&_sem)) {
-			if (errno == EINTR) {
-				errno = 0;
-				continue;
-			} else if (errno == EAGAIN) {
-				return false;
-			} else {
-				return false;
-			}
+		//Returns zero on success, or non-zero if the timeout occurred.
+		if (dispatch_semaphore_wait(_sem, DISPATCH_TIME_NOW) == 0) {
+			_count.decrement();
+			return true;
 		}
 
-		return true;
+		return false;
 	}
 
 	_ALWAYS_INLINE_ int get() const {
-		int val;
-		sem_getvalue(&_sem, &val);
-
-		return val;
+		return _count.get();
 	}
 
 	Semaphore() {
-		int r = sem_init(&_sem, 0, 0);
-
-		ERR_FAIL_COND(r != 0);
+		// Passing zero for the value is useful for when two threads need to reconcile
+		// the completion of a particular event. Passing a value greater than zero is
+		// useful for managing a finite pool of resources, where the pool size is equal
+		// to the value.
+		_sem = dispatch_semaphore_create(0);
 	}
 
 	~Semaphore() {
-		sem_destroy(&_sem);
 	}
 
 private:
-	mutable sem_t _sem;
+	mutable dispatch_semaphore_t _sem;
+	mutable SafeNumeric<uint32_t> _count;
 };
 
 #endif // PLATFORM_SEMAPHORE_H
