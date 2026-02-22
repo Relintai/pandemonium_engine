a62b8a81e6b1a19f7eb89a2f58b7e72eec50643a
Relintai <relintai@relintai.net>
2026-02-22 09:04:19 +0100

    Make the default thread implementation in core the dummy one.

--- core/os/thread.h
+++ core/os/thread.h
@@ -43,11 +43,6 @@
 
 #include "core/typedefs.h"
 
-#if !defined(NO_THREADS)
-#include "core/os/safe_refcount.h"
-#include <thread>
-#endif
-
 class String;
 
 class Thread {
@@ -67,52 +62,6 @@ public:
 		Settings() { priority = PRIORITY_NORMAL; }
 	};
 
-private:
-#if !defined(NO_THREADS)
-	friend class Main;
-
-	static ID main_thread_id;
-
-	static void setup_main_thread_id(ID p_main_thread_id) { main_thread_id = p_main_thread_id; }
-
-	static uint64_t _thread_id_hash(const std::thread::id &p_t);
-
-	ID id = _thread_id_hash(std::thread::id());
-	std::thread thread;
-
-	static void callback(Thread *p_self, const Settings &p_settings, Thread::Callback p_callback, void *p_userdata);
-
-	static Error (*set_name_func)(const String &);
-	static void (*set_priority_func)(Thread::Priority);
-	static void (*init_func)();
-	static void (*term_func)();
-#endif
-
-public:
-	static void _set_platform_funcs(
-			Error (*p_set_name_func)(const String &),
-			void (*p_set_priority_func)(Thread::Priority),
-			void (*p_init_func)() = nullptr,
-			void (*p_term_func)() = nullptr);
-
-#if !defined(NO_THREADS)
-	_FORCE_INLINE_ ID get_id() const { return id; }
-	// get the ID of the caller thread
-	static ID get_caller_id();
-	// get the ID of the main thread
-	_FORCE_INLINE_ static ID get_main_id() { return main_thread_id; }
-
-	_FORCE_INLINE_ static bool is_main_thread() { return get_caller_id() == main_thread_id; }
-
-	static Error set_name(const String &p_name);
-
-	void start(Thread::Callback p_callback, void *p_user, const Settings &p_settings = Settings());
-	bool is_started() const;
-	///< waits until thread is finished, and deallocates it.
-	void wait_to_finish();
-
-	~Thread();
-#else
 	_FORCE_INLINE_ ID get_id() const { return 0; }
 	// get the ID of the caller thread
 	_FORCE_INLINE_ static ID get_caller_id() { return 0; }
@@ -126,7 +75,6 @@ public:
 	void start(Thread::Callback p_callback, void *p_user, const Settings &p_settings = Settings()) {}
 	bool is_started() const { return false; }
 	void wait_to_finish() {}
-#endif
 };
 
 #endif // THREAD_H
