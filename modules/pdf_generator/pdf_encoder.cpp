0a40ea8878787f8f268559e3421842980e3245e8
Relintai <relintai@relintai.net>
2026-03-21 19:07:36 +0100

    Add avaliable encodings to PDFEncoder.

--- modules/pdf_generator/pdf_encoder.cpp
+++ modules/pdf_generator/pdf_encoder.cpp
@@ -35,6 +35,69 @@
 #include "hpdf_doc.h"
 #include "hpdf_pages.h"
 
+String PDFEncoder::get_encoding_string(const Encoding p_encoding) {
+	switch (p_encoding) {
+		case ENCODING_FONT_SPECIFIC:
+			return String("FontSpecific");
+		case ENCODING_STANDARD:
+			return String("StandardEncoding");
+		case ENCODING_MAC_ROMAN:
+			return String("MacRomanEncoding");
+		case ENCODING_WIN_ANSI:
+			return String("WinAnsiEncoding");
+		case ENCODING_ISO8859_2:
+			return String("ISO8859-2");
+		case ENCODING_ISO8859_3:
+			return String("ISO8859-3");
+		case ENCODING_ISO8859_4:
+			return String("ISO8859-4");
+		case ENCODING_ISO8859_5:
+			return String("ISO8859-5");
+		case ENCODING_ISO8859_6:
+			return String("ISO8859-6");
+		case ENCODING_ISO8859_7:
+			return String("ISO8859-7");
+		case ENCODING_ISO8859_8:
+			return String("ISO8859-8");
+		case ENCODING_ISO8859_9:
+			return String("ISO8859-9");
+		case ENCODING_ISO8859_10:
+			return String("ISO8859-10");
+		case ENCODING_ISO8859_11:
+			return String("ISO8859-11");
+		case ENCODING_ISO8859_13:
+			return String("ISO8859-13");
+		case ENCODING_ISO8859_14:
+			return String("ISO8859-14");
+		case ENCODING_ISO8859_15:
+			return String("ISO8859-15");
+		case ENCODING_ISO8859_16:
+			return String("ISO8859-16");
+		case ENCODING_CP1250:
+			return String("CP1250");
+		case ENCODING_CP1251:
+			return String("CP1251");
+		case ENCODING_CP1252:
+			return String("CP1252");
+		case ENCODING_CP1253:
+			return String("CP1253");
+		case ENCODING_CP1254:
+			return String("CP1254");
+		case ENCODING_CP1255:
+			return String("CP1255");
+		case ENCODING_CP1256:
+			return String("CP1256");
+		case ENCODING_CP1257:
+			return String("CP1257");
+		case ENCODING_CP1258:
+			return String("CP1258");
+		case ENCODING_KOI8_R:
+			return String("KOI8-R");
+		default:
+			return String();
+	}
+}
+
 uint32_t PDFEncoder::get_status() {
 	return _status;
 }
@@ -55,7 +118,38 @@ void PDFEncoder::_set_hpdf_encoder(void *p_encoder) {
 }
 
 void PDFEncoder::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("get_encoding_string", "encoding"), &PDFEncoder::get_encoding_string);
+
 	//ClassDB::bind_method(D_METHOD("get_width"), &PDFEncoder::get_width);
 	//ClassDB::bind_method(D_METHOD("set_width", "val"), &PDFEncoder::set_width);
 	//ADD_PROPERTY(PropertyInfo(Variant::REAL, "width"), "set_width", "get_width");
+
+	BIND_ENUM_CONSTANT(ENCODING_FONT_SPECIFIC);
+	BIND_ENUM_CONSTANT(ENCODING_STANDARD);
+	BIND_ENUM_CONSTANT(ENCODING_MAC_ROMAN);
+	BIND_ENUM_CONSTANT(ENCODING_WIN_ANSI);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_2);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_3);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_4);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_5);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_6);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_7);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_8);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_9);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_10);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_11);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_13);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_14);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_15);
+	BIND_ENUM_CONSTANT(ENCODING_ISO8859_16);
+	BIND_ENUM_CONSTANT(ENCODING_CP1250);
+	BIND_ENUM_CONSTANT(ENCODING_CP1251);
+	BIND_ENUM_CONSTANT(ENCODING_CP1252);
+	BIND_ENUM_CONSTANT(ENCODING_CP1253);
+	BIND_ENUM_CONSTANT(ENCODING_CP1254);
+	BIND_ENUM_CONSTANT(ENCODING_CP1255);
+	BIND_ENUM_CONSTANT(ENCODING_CP1256);
+	BIND_ENUM_CONSTANT(ENCODING_CP1257);
+	BIND_ENUM_CONSTANT(ENCODING_CP1258);
+	BIND_ENUM_CONSTANT(ENCODING_KOI8_R);
 }
