7d532bda112b1b4f60f4749473fb34651aa1e00d
Relintai <relintai@relintai.net>
2026-03-25 00:09:01 +0100

    Implement and bind all methods in PDFFont.

--- modules/pdf_generator/pdf_font.cpp
+++ modules/pdf_generator/pdf_font.cpp
@@ -31,17 +31,69 @@
 
 #include "pdf_font.h"
 
-#include "hpdf.h"
-#include "hpdf_doc.h"
-#include "hpdf_pages.h"
+#include "core/object/method_bind_ext.gen.inc"
 
-uint32_t PDFFont::get_status() {
-	return _status;
+#include "hpdf.h"
+
+String PDFFont::get_font_name() {
+	return String::utf8(HPDF_Font_GetFontName((HPDF_Font)_font));
+}
+String PDFFont::get_encoding_name() {
+	return String::utf8(HPDF_Font_GetEncodingName((HPDF_Font)_font));
+}
+
+int PDFFont::get_unicode_width(const uint16_t p_code) {
+	return HPDF_Font_GetUnicodeWidth((HPDF_Font)_font, p_code);
+}
+
+Rect2 PDFFont::get_bbox() {
+	HPDF_Box box = HPDF_Font_GetBBox((HPDF_Font)_font);
+
+	Rect2 r;
+	r.position.x = box.left;
+	r.position.y = box.top;
+	r.set_end(Vector2(box.right, box.bottom));
+
+	return r;
+}
+int PDFFont::get_ascent() {
+	return HPDF_Font_GetAscent((HPDF_Font)_font);
+}
+int PDFFont::get_descent() {
+	return HPDF_Font_GetDescent((HPDF_Font)_font);
+}
+uint32_t PDFFont::get_x_height() {
+	return HPDF_Font_GetXHeight((HPDF_Font)_font);
+}
+uint32_t PDFFont::get_cap_height() {
+	return HPDF_Font_GetCapHeight((HPDF_Font)_font);
+}
+
+Dictionary PDFFont::get_text_width(const String &p_text) {
+	CharString cs = p_text.utf8();
+
+	HPDF_TextWidth tw = HPDF_Font_TextWidth((HPDF_Font)_font, (const HPDF_BYTE *)cs.get_data(), cs.length());
+
+	Dictionary d;
+	d["numchars"] = tw.numchars;
+	d["numwords"] = tw.numwords;
+	d["width"] = tw.width;
+	d["numspace"] = tw.numspace;
+
+	return d;
+}
+Array PDFFont::measure_text(const String &p_text, const float p_width, const float p_font_size, const float p_char_space, const float p_word_space, const bool p_wordwrap) {
+	CharString cs = p_text.utf8();
+	float real_width = 0;
+	uint32_t width = HPDF_Font_MeasureText((HPDF_Font)_font, (const HPDF_BYTE *)cs.get_data(), cs.length(), p_width, p_font_size, p_char_space, p_word_space, p_wordwrap, &real_width);
+
+	Array arr;
+	arr.push_back(width);
+	arr.push_back(real_width);
+	return arr;
 }
 
 PDFFont::PDFFont() {
-	_status = 0;
-
 	_font = NULL;
 }
 
@@ -57,7 +109,17 @@ void PDFFont::_set_hpdf_font(void *p_font) {
 }
 
 void PDFFont::_bind_methods() {
-	//ClassDB::bind_method(D_METHOD("get_width"), &PDFFont::get_width);
-	//ClassDB::bind_method(D_METHOD("set_width", "val"), &PDFFont::set_width);
-	//ADD_PROPERTY(PropertyInfo(Variant::REAL, "width"), "set_width", "get_width");
+	ClassDB::bind_method(D_METHOD("get_font_name"), &PDFFont::get_font_name);
+	ClassDB::bind_method(D_METHOD("get_encoding_name"), &PDFFont::get_encoding_name);
+
+	ClassDB::bind_method(D_METHOD("get_unicode_width", "code"), &PDFFont::get_unicode_width);
+
+	ClassDB::bind_method(D_METHOD("get_bbox"), &PDFFont::get_bbox);
+	ClassDB::bind_method(D_METHOD("get_ascent"), &PDFFont::get_ascent);
+	ClassDB::bind_method(D_METHOD("get_descent"), &PDFFont::get_descent);
+	ClassDB::bind_method(D_METHOD("get_x_height"), &PDFFont::get_x_height);
+	ClassDB::bind_method(D_METHOD("get_cap_height"), &PDFFont::get_cap_height);
+
+	ClassDB::bind_method(D_METHOD("get_text_width", "text"), &PDFFont::get_text_width);
+	ClassDB::bind_method(D_METHOD("measure_text", "text", "width", "font_size", "char_space", "word_space", "wordwrap"), &PDFFont::measure_text);
 }
--- modules/pdf_generator/pdf_font.h
+++ modules/pdf_generator/pdf_font.h
@@ -38,50 +38,19 @@ class PDFFont : public Reference {
 	GDCLASS(PDFFont, Reference);
 
 public:
-#if 0
-	HPDF_EXPORT(const char *)
-	HPDF_Font_GetFontName(HPDF_Font font);
+	String get_font_name();
+	String get_encoding_name();
 
-	HPDF_EXPORT(const char *)
-	HPDF_Font_GetEncodingName(HPDF_Font font);
+	int get_unicode_width(const uint16_t p_code);
 
-	HPDF_EXPORT(HPDF_INT)
-	HPDF_Font_GetUnicodeWidth(HPDF_Font font,
-			HPDF_UNICODE code);
+	Rect2 get_bbox();
+	int get_ascent();
+	int get_descent();
+	uint32_t get_x_height();
+	uint32_t get_cap_height();
 
-	HPDF_EXPORT(HPDF_Box)
-	HPDF_Font_GetBBox(HPDF_Font font);
-
-	HPDF_EXPORT(HPDF_INT)
-	HPDF_Font_GetAscent(HPDF_Font font);
-
-	HPDF_EXPORT(HPDF_INT)
-	HPDF_Font_GetDescent(HPDF_Font font);
-
-	HPDF_EXPORT(HPDF_UINT)
-	HPDF_Font_GetXHeight(HPDF_Font font);
-
-	HPDF_EXPORT(HPDF_UINT)
-	HPDF_Font_GetCapHeight(HPDF_Font font);
-
-	HPDF_EXPORT(HPDF_TextWidth)
-	HPDF_Font_TextWidth(HPDF_Font font,
-			const HPDF_BYTE *text,
-			HPDF_UINT len);
-
-	HPDF_EXPORT(HPDF_UINT)
-	HPDF_Font_MeasureText(HPDF_Font font,
-			const HPDF_BYTE *text,
-			HPDF_UINT len,
-			HPDF_REAL width,
-			HPDF_REAL font_size,
-			HPDF_REAL char_space,
-			HPDF_REAL word_space,
-			HPDF_BOOL wordwrap,
-			HPDF_REAL *real_width);
-#endif
-
-	uint32_t get_status();
+	Dictionary get_text_width(const String &p_text);
+	Array measure_text(const String &p_text, const float p_width, const float p_font_size, const float p_char_space, const float p_word_space, const bool p_wordwrap);
 
 	PDFFont();
 	~PDFFont();
@@ -93,8 +62,6 @@ protected:
 	static void _bind_methods();
 
 	void *_font;
-
-	uint32_t _status;
 };
 
 #endif
