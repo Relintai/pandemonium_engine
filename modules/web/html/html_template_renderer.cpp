365ebb1a03cc01721fc022c3644c2641390c5ba9
Relintai <relintai@relintai.net>
2026-03-27 20:16:37 +0100

    Initial HTMLTemplaterenderer setup.

--- modules/web/html/html_template_renderer.cpp
+++ modules/web/html/html_template_renderer.cpp
@@ -29,7 +29,81 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
+// New builtin funcs:
+// exists(<var name>)
+// equals(<var1>, <var2>)
+// approx_equals(<var1>, <var2>)
+// mod(<var1>, <var2>)
+// set(<key (str), no need for "">, <value>)  ret value  (if value is null, unset)
+// set_if(<bool expr>, <key (str), no need for "">, <value>)  ret value  (if value is null, unset)
+// int(), float(), str()
+// range(<var1>, <var2 opt>, <var 3 opt>)
+// keys(<var1 Dict>)
+// values(<var1 Dict>)
+
+// No assignment operator, set should be good enough
+
+// operators: All variant ops including in  (not that it can't do assignment)
+// ternary expression:
+// {{? <bool expr>; <true expr>; <false expr> }}
+// Could also just parse: Equivalent to qr, qp etc (so if false expr is null, just emit "", same for true expr)
+// {{? <bool expr>; <true expr> }}
+// Could also just parse: Equivalent to qr, qp etc (so if false expr is null, just emit "", same for true expr)
+// {{? <bool expr>;; <false expr> }}
+// assignment? Just assign to the input dictionary
+
+// if? If there are bool expressions and ternary, an if should probably also be here
+// It should still be relatively simple, as there are no scopes, and no real proper variable declarations, and stack
+// {{if <bool expr> }}
+// {{elif <bool expr> }}
+// {{else}}
+// {{endif}}
+
+// foreach?
+// It does need variable declaration, but it could be just added to the input dictionary.
+// Also with the range built in, this could also double as normal for like in gdscript
+// {{for <variable declaration> in <collection> }}
+// {{endfor}}
+// Variant can do the iteration, that mechanism should just be used.
+
+// other stuff:
+// Recursive indexing now should work
+
+// Variant call / Object call?
+// likely not a good idea, skip
+
+// Array and dictionary construction?
+// No need, do arrays and dicts in template scripts
+
 #include "html_template_renderer.h"
+
+String HTMLTemplaterenderer::render(const Dictionary &p_data, const bool p_show_error) {
+	ERR_FAIL_COND_V_MSG(_error_set, String(), "There was previously a parse error: " + _error_str + ".");
+
+	Dictionary data = p_data;
+	StringBuilder html;
+
+	_execution_error = false;
+	Variant output;
+	String error_txt;
+	bool err = _execute(data, html, _root, output, error_txt);
+
+	if (err) {
+		_execution_error = true;
+		_error_str = error_txt;
+		ERR_FAIL_COND_V_MSG(p_show_error, html.as_string(), _error_str);
+	}
+
+	return html.as_string();
+}
+
+bool HTMLTemplaterenderer::compile(const String &p_text) {
+	return false;
+}
+
+String HTMLTemplaterenderer::get_error_str() {
+	return _error_str;
+}
 
 HTMLTemplaterenderer::HTMLTemplaterenderer() {
 }
@@ -38,5 +112,46 @@ HTMLTemplaterenderer::~HTMLTemplaterenderer() {
 }
 
 void HTMLTemplaterenderer::_bind_methods() {
-	//ClassDB::bind_method(D_METHOD("has_template", "name"), &HTMLTemplaterenderer::has_template);
+	ClassDB::bind_method(D_METHOD("render", "data", "p_show_error"), &HTMLTemplaterenderer::render, DEFVAL(true));
+	ClassDB::bind_method(D_METHOD("compile", "text"), &HTMLTemplaterenderer::compile);
+	ClassDB::bind_method(D_METHOD("get_error_str"), &HTMLTemplaterenderer::get_error_str);
+}
+
+#if 0
+static bool _is_number(CharType c) {
+	return (c >= '0' && c <= '9');
+}
+
+static bool _is_hex_digit(char32_t c) {
+	return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
+}
+
+static bool _is_binary_digit(char32_t c) {
+	return (c == '0' || c == '1');
+}
+#endif
+
+const char *HTMLTemplaterenderer::func_name[HTMLTemplaterenderer::FUNC_MAX] = {
+	"p",
+	"pr",
+	"pb",
+	"prb",
+	"vf",
+	"qp",
+	"qpr",
+	"qpb",
+	"qprb",
+	"qvf",
+	"exists",
+	"equals",
+	"approx_equals",
+};
+
+const char *HTMLTemplaterenderer::token_name[TK_MAX] = {
+	"EOF",
+	"ERROR",
+};
+
+bool HTMLTemplaterenderer::_execute(Dictionary &p_data, StringBuilder &p_html, TNode *p_node, Variant &r_ret, String &r_error_str) {
+	return false;
 }
--- modules/web/html/html_template_renderer.h
+++ modules/web/html/html_template_renderer.h
@@ -32,17 +32,184 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
+#include "core/string/string_builder.h"
+
 #include "core/object/resource.h"
 
 class HTMLTemplaterenderer : public Reference {
 	GDCLASS(HTMLTemplaterenderer, Reference);
 
 public:
+	String render(const Dictionary &p_data, const bool p_show_error = true);
+	bool compile(const String &p_text);
+
+	String get_error_str();
+
 	HTMLTemplaterenderer();
 	~HTMLTemplaterenderer();
 
 protected:
 	static void _bind_methods();
+
+protected:
+	enum BuiltinFunc {
+		FUNC_PRINT,
+		FUNC_PRINT_RAW,
+		FUNC_PRINT_BR,
+		FUNC_PRINT_RAW_BR,
+		FUNC_VFORMAT,
+		FUNC_QPRINT,
+		FUNC_QPRINT_RAW,
+		FUNC_QPRINT_BR,
+		FUNC_QPRINT_RAW_BR,
+		FUNC_QVFORMAT,
+		FUNC_EXISTS,
+		FUNC_EQUALS,
+		FUNC_APPROX_EQUALS,
+		FUNC_MAX
+	};
+
+	static const char *func_name[FUNC_MAX];
+
+	enum TokenType {
+		TK_EOF,
+		TK_ERROR,
+		TK_MAX
+	};
+
+	static const char *token_name[TK_MAX];
+
+	struct Token {
+		TokenType type;
+		Variant value;
+	};
+
+	String _error_str;
+	bool _error_set;
+
+	struct TNode {
+		enum Type {
+			TYPE_HTML_DATA,
+			TYPE_CONSTANT,
+			TYPE_INDEX,
+			TYPE_NAMED_INDEX,
+			TYPE_BUILTIN_FUNC,
+			TYPE_OPERATOR,
+			TYPE_IF,
+			TYPE_FOREACH,
+		};
+
+		TNode *next;
+
+		Type type;
+
+		TNode() {
+			type = Type::TYPE_CONSTANT;
+			next = nullptr;
+		}
+		virtual ~TNode() {
+			if (next) {
+				memdelete(next);
+			}
+		}
+	};
+
+	struct HTMLDataNode : public TNode {
+		String value;
+		HTMLDataNode() {
+			type = TYPE_HTML_DATA;
+		}
+	};
+
+	struct ConstantNode : public TNode {
+		Variant value;
+		ConstantNode() {
+			type = TYPE_CONSTANT;
+		}
+	};
+
+	struct IndexNode : public TNode {
+		TNode *base;
+		TNode *index;
+
+		IndexNode() {
+			type = TYPE_INDEX;
+			base = nullptr;
+			index = nullptr;
+		}
+	};
+
+	struct NamedIndexNode : public TNode {
+		TNode *base;
+		StringName name;
+
+		NamedIndexNode() {
+			type = TYPE_NAMED_INDEX;
+			base = nullptr;
+		}
+	};
+
+	struct BuiltinFuncNode : public TNode {
+		BuiltinFunc func;
+		Vector<TNode *> arguments;
+		BuiltinFuncNode() {
+			type = TYPE_BUILTIN_FUNC;
+			func = BuiltinFunc::FUNC_PRINT;
+		}
+	};
+
+	struct OperatorNode : public TNode {
+		Variant::Operator op;
+
+		TNode *nodes[2];
+
+		OperatorNode() {
+			type = TYPE_OPERATOR;
+			nodes[0] = nullptr;
+			nodes[1] = nullptr;
+		}
+	};
+
+	struct IfNode : public TNode {
+		TNode *condition;
+		TNode *body;
+		TNode *body_else;
+
+		IfNode() {
+			type = TYPE_IF;
+			condition = nullptr;
+			body = nullptr;
+			body_else = nullptr;
+		}
+	};
+
+	struct ForeachNode : public TNode {
+		TNode *condition;
+		TNode *body;
+
+		ForeachNode() {
+			type = TYPE_FOREACH;
+			condition = nullptr;
+			body = nullptr;
+		}
+	};
+
+	template <class T>
+	T *alloc_node() {
+		T *node = memnew(T);
+		node->next = _nodes;
+		_nodes = node;
+		return node;
+	}
+
+	TNode *_root;
+	TNode *_nodes;
+
+	String _text;
+
+	bool _execution_error;
+
+	bool _execute(Dictionary &p_data, StringBuilder &p_html, TNode *p_node, Variant &r_ret, String &r_error_str);
 };
 
 #endif
