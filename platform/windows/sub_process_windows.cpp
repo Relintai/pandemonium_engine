5f4abd22e0c8382f89fc3489f42bf5919577adde
Relintai <relintai@relintai.net>
2026-09-01 16:31:39 +0200

    Added is_process_running() check in SubProcessWindows::poll() if polling from an another thread while blocking.

--- platform/windows/sub_process_windows.cpp
+++ platform/windows/sub_process_windows.cpp
@@ -363,6 +363,11 @@ Error SubProcessWindows::poll() {
 	}
 
 	if (_blocking) {
+		// This should the api more convenient to use.
+		if (!is_process_running()) {
+			return ERR_FILE_EOF;
+		}
+
 		// If it's blocking, and we want to read output from an another thread, we can just do it without poll
 		// Just ignore poll calls
 		return OK;
