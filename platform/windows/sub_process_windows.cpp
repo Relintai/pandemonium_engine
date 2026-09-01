b7746689241fdc0c5f945221e0a6b25fe234fcb1
Relintai <relintai@relintai.net>
2026-09-01 10:14:15 +0200

    Fix typo.

--- platform/windows/sub_process_windows.cpp
+++ platform/windows/sub_process_windows.cpp
@@ -243,7 +243,7 @@ Error SubProcessWindows::start() {
 				const int bytes_to_convert = err_bytes_in_buffer + (newline_index + 1);
 				_append_to_std_err(_bytes.ptr(), bytes_to_convert);
 
-				err_bytes_in_buffer = read - (newline_index + 1);
+				err_bytes_in_buffer = err_read - (newline_index + 1);
 				memmove(_err_bytes.ptr(), _err_bytes.ptr() + bytes_to_convert, bytes_in_buffer);
 			}
 
